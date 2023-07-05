628f1b78a601d98dae5956336695d88a0e43f0d3
Shintaro SAKODA <shintaro.sakoda@tier4.jp>
2023-07-05 14:45:40 +0900

    Added sample_test.sh
    Signed-off-by: Shintaro SAKODA <shintaro.sakoda@tier4.jp>

---
+++ sample_test.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+set -eux
+
+# スクリプトが終了する際にすべてのバックグラウンドプロセスを停止するためのトラップを設定
+trap "kill 0" EXIT
+
+# ノードをバックグラウンドで起動
+ros2 launch my_package my_node.launch.py &
+
+# レコードをバックグラウンドで起動
+ros2 bag record -a -o output.bag &
+
+# バッグファイルのリプレイ（これはフォアグラウンドで実行し、終了すると次の行に進む）
+ros2 bag play input.bag
+
+# すべてのバックグラウンドプロセスを停止
+kill 0
+
+# ここでoutput.bagを適当にパースして評価値を出力する
+python3 parse_rosbag_output.py
