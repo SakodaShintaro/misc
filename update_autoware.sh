5ad5e55229be4345dcec97295662a61ce12558b2
Shintaro SAKODA <shintaro.sakoda@tier4.jp>
2023-07-12 22:33:57 +0900

    Added export src
    Signed-off-by: Shintaro SAKODA <shintaro.sakoda@tier4.jp>

--- update_autoware.sh
+++ update_autoware.sh
@@ -5,6 +5,7 @@ set -eux
 git pull
 vcs import src < autoware.repos
 vcs pull src
+vcs export src --exact > my_autoware.repos
 rosdep update
 rosdep install -y --from-paths src --ignore-src --rosdistro $ROS_DISTRO
 colcon build --symlink-install --cmake-args -DCMAKE_BUILD_TYPE=Release
