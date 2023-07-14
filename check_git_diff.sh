b66588925059b2e5c14adb1805d1af7cea2d8c5c
Shintaro SAKODA <shintaro.sakoda@tier4.jp>
2023-07-14 09:08:35 +0900

    Added check_git_diff.sh
    Signed-off-by: Shintaro SAKODA <shintaro.sakoda@tier4.jp>

---
+++ check_git_diff.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+function check_git_diff() {
+  local dir=$1
+  if [ -d "$dir/.git" ]; then
+    # echo "Checking $dir"
+    git_diff_output=$(git -C "$dir" diff)
+
+    if [ -n "$git_diff_output" ]; then
+      echo "Diff found in $(readlink -f $dir)"
+    #   echo $git_diff_output
+    fi
+  fi
+}
+
+function traverse() {
+  for d in "$1"/*; do
+    if [ -d "$d" ]; then
+      check_git_diff "$d"
+      traverse "$d"
+    fi
+  done
+}
+
+# 再帰的に調査
+traverse "./"
