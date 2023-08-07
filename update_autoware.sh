814f8c82f44b027b2b9f066f70f8ddd32b10ad03
Shintaro Sakoda <shintaro.sakoda@tier4.jp>
2023-08-07 14:59:38 +0900

    Added autoware.rviz
    Signed-off-by: Shintaro Sakoda <shintaro.sakoda@tier4.jp>

--- update_autoware.sh
+++ update_autoware.sh
@@ -10,3 +10,4 @@ vcs export src --exact > my_autoware.repos
 rosdep update
 rosdep install -y --from-paths src --ignore-src --rosdistro $ROS_DISTRO
 MAKEFLAGS="-j1" colcon build --symlink-install --cmake-args -DCMAKE_BUILD_TYPE=Release
+cp $(dirname $0)/autoware.rviz ./src/launcher/autoware_launch/autoware_launch/rviz/autoware.rviz
