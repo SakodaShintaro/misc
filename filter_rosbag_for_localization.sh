9b0133a2d5ccfc3460b404015fd51e49c40afc79
Shintaro Sakoda <shintaro.sakoda@tier4.jp>
2023-08-10 09:51:16 +0900

    Added filter_rosbag_for_localization.sh
    Signed-off-by: Shintaro Sakoda <shintaro.sakoda@tier4.jp>

---
+++ filter_rosbag_for_localization.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+set -eux
+
+# 読み込み
+set +eux
+source $HOME/autoware/install/setup.bash
+set -eux
+
+TARGET=$(readlink -f $1)
+
+ros2 bag filter ${TARGET} -o ${TARGET}_filtered -x "/localization/.*" "/sensing/lidar/concatenated/pointcloud" "/tf"
