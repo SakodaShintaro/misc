81e245b261faf109845d0201144a6813d2964036
Shintaro Sakoda <shintaro.sakoda@tier4.jp>
2023-08-18 14:11:42 +0900

    update update_autoware.sh
    Signed-off-by: Shintaro Sakoda <shintaro.sakoda@tier4.jp>

--- update_autoware.sh
+++ update_autoware.sh
@@ -9,5 +9,5 @@ vcs pull src
 vcs export src --exact > my_autoware.repos
 rosdep update
 rosdep install -y --from-paths src --ignore-src --rosdistro $ROS_DISTRO
-MAKEFLAGS="-j1" colcon build --symlink-install --cmake-args -DCMAKE_BUILD_TYPE=Release
+MAKEFLAGS="-j2" colcon build --symlink-install --cmake-args -DCMAKE_BUILD_TYPE=Release -DBUILD_TESTING=OFF
 cp $(dirname $0)/autoware.rviz ./src/launcher/autoware_launch/autoware_launch/rviz/autoware.rviz
