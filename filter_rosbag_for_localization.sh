a5e26ddba8f759ed6162e868458ab6f68a8c8eb6
Shintaro SAKODA <shintaro.sakoda@tier4.jp>
2023-08-23 18:22:46 +0900

    Fixed filter for awsim
    Signed-off-by: Shintaro SAKODA <shintaro.sakoda@tier4.jp>

--- filter_rosbag_for_localization.sh
+++ filter_rosbag_for_localization.sh
@@ -9,10 +9,22 @@ set -eux
 
 TARGET=$(readlink -f $1)
 
-ros2 bag filter ${TARGET} -o ${TARGET}_filtered -x \
-    "/planning/.*" \
-    "/control/.*" \
-    "/perception/.*" \
-    "/localization/.*" \
-    "/sensing/lidar/concatenated/pointcloud" \
-    "/tf"
+# ros2 bag filter ${TARGET} -o ${TARGET}_filtered -x \
+#     "/planning/.*" \
+#     "/control/.*" \
+#     "/perception/.*" \
+#     "/localization/.*" \
+#     "/sensing/lidar/concatenated/pointcloud" \
+#     "/tf"
+
+ros2 bag filter ${TARGET} -o ${TARGET}_filtered --include \
+    "/sensing/camera/traffic_light/camera_info" \
+    "/sensing/camera/traffic_light/image_raw" \
+    "/sensing/gnss/pose" \
+    "/sensing/gnss/pose_with_covariance" \
+    "/sensing/imu/tamagawa/imu_raw" \
+    "/sensing/lidar/top/pointcloud_raw" \
+    "/sensing/lidar/top/pointcloud_raw_ex" \
+    "/awsim/ground_truth/localization/kinematic_state" \
+    "/awsim/ground_truth/vehicle/pose" \
+    "/clock"
