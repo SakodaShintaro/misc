c2634d0dc34978742ddf57d08a48e8d2dfdd76d9
Shintaro Sakoda <sakoda@keio.jp>
2023-09-03 17:10:40 +0900

    Added publish_initial_pose_in_awsim.sh
    Signed-off-by: Shintaro Sakoda <sakoda@keio.jp>

---
+++ publish_initialpose_in_awsim.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+set -eu
+
+TOPIC_NAME="/localization/initialization_state"
+
+# トピックがpublishされるまで待機
+while ! ros2 topic list | grep -q "$TOPIC_NAME"; do
+  echo "Waiting for $TOPIC_NAME to be published..."
+  sleep 1.0
+done
+
+ros2 topic pub -1 /initialpose geometry_msgs/msg/PoseWithCovarianceStamped '{
+  header: { frame_id: "map" },
+  pose: {
+    pose: {
+      position: { x: 81377.34375, y: 49916.89453125, z: 41.205413818359375 },
+      orientation: { x: 0.00017802136426325887, y: -0.007339125499129295, z: 0.3006163239479065, w: 0.9537169337272644 }
+    },
+    covariance: [0.25, 0.0,  0.0, 0.0, 0.0, 0.0,
+                 0.0,  0.25, 0.0, 0.0, 0.0, 0.0,
+                 0.0,  0.0,  0.0, 0.0, 0.0, 0.0,
+                 0.0,  0.0,  0.0, 0.0, 0.0, 0.0,
+                 0.0,  0.0,  0.0, 0.0, 0.0, 0.0,
+                 0.0,  0.0,  0.0, 0.0, 0.0, 0.06853891909122467]
+  }
+}'
