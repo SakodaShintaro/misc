1a1b41d22a147b2adc027763f49f383dc9603006
Shintaro SAKODA <shintaro.sakoda@tier4.jp>
2023-09-04 12:56:39 +0900

    Fixed trigger call
    Signed-off-by: Shintaro SAKODA <shintaro.sakoda@tier4.jp>

--- ros2/script/exec_log_sim_and_evaluation.sh
+++ ros2/script/exec_log_sim_and_evaluation.sh
@@ -33,7 +33,7 @@ ros2 launch autoware_launch logging_simulator.launch.xml \
 
 # 立ち上がるまで待つ
 # サービス呼び出しを立ち上がりの確認とする
-ros2 service call /localization/pose_estimator/trigger_node std_srvs/srv/SetBool "{data: false}"
+ros2 service call /localization/pose_twist_fusion_filter/trigger_node std_srvs/srv/SetBool "{data: false}"
 
 # 安定性のため少し待つ
 sleep 5
