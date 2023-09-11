599426082a31a2b562a3ede0971a86c236d6d4d3
Shintaro Sakoda <shintaro.sakoda@tier4.jp>
2023-09-12 08:13:56 +0900

    Added AWSIM initial GNSS(temp)
    Signed-off-by: Shintaro Sakoda <shintaro.sakoda@tier4.jp>

--- publish_initialpose_in_awsim.sh
+++ publish_initialpose_in_awsim.sh
@@ -24,3 +24,20 @@ ros2 topic pub -1 /initialpose geometry_msgs/msg/PoseWithCovarianceStamped '{
                  0.0,  0.0,  0.0, 0.0, 0.0, 0.06853891909122467]
   }
 }'
+
+# AWSIM initial GNSS
+# ros2 topic pub -1 /initialpose geometry_msgs/msg/PoseWithCovarianceStamped '{
+#   header: { frame_id: "map" },
+#   pose: {
+#     pose: {
+#       position: { x: 81377.976562, y: 49917.347656, z: 43.032684 },
+#       orientation: { x: 0.000000, y: 0.000000, z: 0.500000, w: 0.000000 }
+#     },
+#     covariance: [1.0, 0.0, 0.0,  0.0,  0.0,  0.0,
+#                  0.0, 1.0, 0.0,  0.0,  0.0,  0.0,
+#                  0.0, 0.0, 0.01, 0.0,  0.0,  0.0,
+#                  0.0, 0.0, 0.0,  0.01, 0.0,  0.0,
+#                  0.0, 0.0, 0.0,  0.0,  0.01, 0.0,
+#                  0.0, 0.0, 0.0,  0.0,  0.0,  10.0]
+#   }
+# }'
