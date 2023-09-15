96092199df1c7af0118d99ada465f70918e09f6d
Shintaro Sakoda <shintaro.sakoda@tier4.jp>
2023-09-15 09:42:39 +0900

    Added 99 try
    Signed-off-by: Shintaro Sakoda <shintaro.sakoda@tier4.jp>

--- call_pose_initialize_service.sh
+++ call_pose_initialize_service.sh
@@ -10,4 +10,9 @@ REQUEST_DATA="{
   pose: []
 }"
 
-ros2 service call /localization/initialize autoware_adapi_v1_msgs/srv/InitializeLocalization "$REQUEST_DATA"
+# 99回、3秒おきにサービスを呼び出す
+for i in {1..99}; do
+    echo "Sending request $i..."
+    ros2 service call /localization/initialize autoware_adapi_v1_msgs/srv/InitializeLocalization "$REQUEST_DATA"
+    sleep 3
+done
