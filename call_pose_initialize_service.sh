6f9adf72c6c58ffa4ee658140d3622590c6b3506
Shintaro Sakoda <shintaro.sakoda@tier4.jp>
2023-09-22 09:44:20 +0900

    Fixed call_pose_initialize_service.sh
    Signed-off-by: Shintaro Sakoda <shintaro.sakoda@tier4.jp>

--- call_pose_initialize_service.sh
+++ call_pose_initialize_service.sh
@@ -10,9 +10,8 @@ REQUEST_DATA="{
   pose: []
 }"
 
-# 99回、3秒おきにサービスを呼び出す
 for i in {1..99}; do
     echo "Sending request $i..."
     ros2 service call /localization/initialize autoware_adapi_v1_msgs/srv/InitializeLocalization "$REQUEST_DATA"
-    sleep 3
+    sleep 1
 done
