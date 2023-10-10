9961547a3e960368cce095204f4381f5158d231e
Shintaro Sakoda <shintaro.sakoda@tier4.jp>
2023-10-10 15:13:32 +0900

    Added test_one_package.sh
    Signed-off-by: Shintaro Sakoda <shintaro.sakoda@tier4.jp>

---
+++ test_one_package.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+set -eux
+
+colcon build --symlink-install --cmake-args -DCMAKE_BUILD_TYPE=Release -DBUILD_TESTING=ON --packages-select $1
+colcon test --packages-select $1
+colcon test-result --all --verbose
