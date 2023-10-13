64d6c71a2f49efbb00fa94887157f11a7ac70579
Shintaro Sakoda <shintaro.sakoda@tier4.jp>
2023-10-13 13:46:24 +0900

    Added filter
    Signed-off-by: Shintaro Sakoda <shintaro.sakoda@tier4.jp>

--- test_one_package.sh
+++ test_one_package.sh
@@ -3,4 +3,4 @@ set -eux
 
 colcon build --symlink-install --cmake-args -DCMAKE_BUILD_TYPE=Release -DBUILD_TESTING=ON --packages-select $1
 colcon test --packages-select $1
-colcon test-result --all --verbose
+colcon test-result --all --verbose | grep $1
