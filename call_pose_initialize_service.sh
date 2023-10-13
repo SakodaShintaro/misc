dc3c8aeb3184935bc73f574e990eb6749829e539
Shintaro Sakoda <shintaro.sakoda@tier4.jp>
2023-10-13 09:08:47 +0900

    Fixed call service script
    Signed-off-by: Shintaro Sakoda <shintaro.sakoda@tier4.jp>

--- call_pose_initialize_service.sh
+++ call_pose_initialize_service.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 set -eux
 
+NUM=$1
+
 # 読み込み
 set +eux
 source $HOME/autoware/install/setup.bash
@@ -10,7 +12,7 @@ REQUEST_DATA="{
   pose: []
 }"
 
-for i in {1..99}; do
+for i in $(seq 1 $NUM); do
     echo "Sending request $i..."
     ros2 service call /localization/initialize autoware_adapi_v1_msgs/srv/InitializeLocalization "$REQUEST_DATA"
     sleep 1
