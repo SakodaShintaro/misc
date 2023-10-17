4d7c923eb4fc3824339a39434d78c84d0e541688
Shintaro Sakoda <shintaro.sakoda@tier4.jp>
2023-10-17 17:10:20 +0900

    Fixed pilot.auto to pilot-auto
    Signed-off-by: Shintaro Sakoda <shintaro.sakoda@tier4.jp>

--- reset_autoware.sh
+++ reset_autoware.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
-# 現状のディレクトリがautowareまたはpilot.autoというプレフィックスを持つことを確認する
+# 現状のディレクトリがautowareまたはpilot-autoというプレフィックスを持つことを確認する
 current_dir=$(basename $(pwd))
-if [[ ! $current_dir =~ ^(autoware|pilot\.auto) ]]; then
-    echo "This script must be run in a directory with a prefix of autoware or pilot.auto."
+if [[ ! $current_dir =~ ^(autoware|pilot-auto) ]]; then
+    echo "This script must be run in a directory with a prefix of autoware or pilot-auto."
     exit 1
 fi
 
