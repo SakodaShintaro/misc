08eed60f18872ac98e5392b9be5d16999ba74491
Shintaro SAKODA <shintaro.sakoda@tier4.jp>
2023-10-18 15:34:29 +0900

    Added select
    Signed-off-by: Shintaro SAKODA <shintaro.sakoda@tier4.jp>

---
+++ build_one_package-select.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+set -eux
+
+# 現状のディレクトリがautowareまたはpilot-autoというプレフィックスを持つことを確認する
+current_dir=$(basename $(pwd))
+if [[ ! $current_dir =~ ^(autoware|pilot-auto) ]]; then
+    echo "This script must be run in a directory with a prefix of autoware or pilot-auto."
+    exit 1
+fi
+
+colcon build --symlink-install --cmake-args -DCMAKE_BUILD_TYPE=Release --packages-select $1
