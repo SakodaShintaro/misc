17c7376d72991914c013c6c56194609d47719692
Shintaro Sakoda <shintaro.sakoda@tier4.jp>
2023-10-27 13:59:24 +0900

    Added lcov
    Signed-off-by: Shintaro Sakoda <shintaro.sakoda@tier4.jp>

--- test_one_package.sh
+++ test_one_package.sh
@@ -1,6 +1,16 @@
 #!/bin/bash
 set -eux
 
-colcon build --symlink-install --cmake-args -DCMAKE_BUILD_TYPE=Release -DBUILD_TESTING=ON --packages-select $1
+# [need]
+# sudo apt-get install -y lcov python3-colcon-lcov-result
+
+colcon build --symlink-install \
+  --cmake-args \
+    -DCMAKE_BUILD_TYPE=Release \
+    -DBUILD_TESTING=ON \
+    -DCMAKE_CXX_FLAGS='-fprofile-arcs -ftest-coverage' \
+    -DCMAKE_C_FLAGS='-fprofile-arcs -ftest-coverage' \
+  --packages-select $1
 colcon test --packages-select $1
+colcon lcov-result --packages-select $1
 colcon test-result --all --verbose | grep $1
