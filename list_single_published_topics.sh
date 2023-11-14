b910fffc3dfcdec8aebc1a14320d3d93cc8b9f5f
Shintaro Sakoda <shintaro.sakoda@tier4.jp>
2023-11-14 09:47:31 +0900

    Added list_single_published_topics.sh
    Signed-off-by: Shintaro Sakoda <shintaro.sakoda@tier4.jp>

---
+++ list_single_published_topics.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+TOPIC_LIST=$(ros2 topic list)
+
+for topic in $TOPIC_LIST; do
+  msg=$(ros2 topic info -v $topic)
+  # msgに
+  #   Publisher count: 1
+  #   Node name: rosbag2_player
+  # というメッセージがある場合のみ表示
+  if [[ $msg =~ "Publisher count: 1" ]] && [[ $msg =~ "Node name: rosbag2_player" ]]; then
+    echo "  $topic \\"
+  fi
+
+done
