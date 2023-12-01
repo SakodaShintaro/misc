77301bb3a2aaef9477ffb5dd5630f766540c8788
Shintaro SAKODA <shintaro.sakoda@tier4.jp>
2023-12-01 10:05:04 +0900

    Added check_launch_time.sh
    Signed-off-by: Shintaro SAKODA <shintaro.sakoda@tier4.jp>

---
+++ check_launch_time.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+set -eux
+
+# スクリプトが終了する際にすべてのバックグラウンドプロセスを停止するためのトラップを設定
+trap "kill 0" EXIT
+
+# 読み込み
+set +eux
+source $HOME/autoware/install/setup.bash
+set -eux
+
+# Autowareをバックグラウンドで起動
+ros2 launch autoware_launch logging_simulator.launch.xml \
+    map_path:=$HOME/autoware_map/sample-map-rosbag \
+    vehicle_model:=sample_vehicle \
+    sensor_model:=sample_sensor_kit &
+
+# 立ち上がるまで待つ
+# サービス呼び出しを立ち上がりの確認とする
+ros2 service call /localization/pose_twist_fusion_filter/trigger_node std_srvs/srv/SetBool "{data: false}"
+
+# 経過時間の計算
+echo "${SECONDS}秒"
