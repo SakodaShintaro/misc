ef80ee67b8f1306743ddcfb160f55dc6432b56b8
Shintaro Sakoda <shintaro.sakoda@tier4.jp>
2024-01-11 11:55:54 +0900

    Fixed check_timestamp.py
    Signed-off-by: Shintaro Sakoda <shintaro.sakoda@tier4.jp>

--- python_lib/check_timestamp.py
+++ python_lib/check_timestamp.py
@@ -6,7 +6,7 @@ from rosbags.rosbag2 import Reader
 from rosbags.serde import deserialize_cdr
 import pandas as pd
 import matplotlib.pyplot as plt
-import os
+import numpy as np
 
 
 def parse_args():
@@ -23,22 +23,26 @@ if __name__ == "__main__":
 
     reader = Reader(rosbag_path)
     reader.open()
-    df = pd.DataFrame(columns=['timestamp'])
+    timestamp_rosbag_list = []
+    timestamp_msg_list = []
     for connection, timestamp, raw_data in reader.messages():
         topic_name = connection.topic
         if topic_name != target_topic:
             continue
         msg = deserialize_cdr(raw_data, connection.msgtype)
-        df = df.append({'timestamp': timestamp}, ignore_index=True)
-        if len(df) >= 500:
-            break
+        timestamp /= 1e9
+        timestamp_msg = msg.header.stamp
+        timestamp_msg = timestamp_msg.sec + timestamp_msg.nanosec / 1e9
+        timestamp_rosbag_list.append(timestamp)
+        timestamp_msg_list.append(timestamp_msg)
 
-    print(df.head())
+    timestamp_msg_list = np.array(timestamp_msg_list)
+    timestamp_rosbag_list = np.array(timestamp_rosbag_list)
+    diff = (timestamp_rosbag_list - timestamp_msg_list) * 1000
 
-    # plot differential
-    plt.plot(df['timestamp'].diff() / 1e9, label='timestamp')
+    # plot
+    plt.plot(diff, label='timestamp')
     plt.title(target_topic)
     plt.xlabel("Frame number")
-    plt.ylabel("diff timestamp[sec]")
-    plt.legend()
+    plt.ylabel("diff timestamp[msec]")
     plt.show()
