1b0763881be0b2ca8c3cac4665e484057520a5ec
Shintaro Sakoda <shintaro.sakoda@tier4.jp>
2024-02-28 10:02:36 +0900

    Added save as tsv
    Signed-off-by: Shintaro Sakoda <shintaro.sakoda@tier4.jp>

--- python_lib/plot_exe_time_ms.py
+++ python_lib/plot_exe_time_ms.py
@@ -19,7 +19,8 @@ if __name__ == "__main__":
     args = parse_args()
     rosbag_path = args.rosbag_path
     serialization_format = "cdr"
-    storage_options = rosbag2_py.StorageOptions(uri=str(rosbag_path), storage_id="sqlite3")
+    storage_options = rosbag2_py.StorageOptions(
+        uri=str(rosbag_path), storage_id="sqlite3")
     converter_options = rosbag2_py.ConverterOptions(
         input_serialization_format=serialization_format,
         output_serialization_format=serialization_format,
@@ -29,7 +30,8 @@ if __name__ == "__main__":
     reader.open(storage_options, converter_options)
 
     topic_types = reader.get_all_topics_and_types()
-    type_map = {topic_types[i].name: topic_types[i].type for i in range(len(topic_types))}
+    type_map = {
+        topic_types[i].name: topic_types[i].type for i in range(len(topic_types))}
 
     target_topics = [
         "/localization/pose_estimator/exe_time_ms",
@@ -46,7 +48,8 @@ if __name__ == "__main__":
         msg_type = get_message(type_map[topic])
         msg = deserialize_message(data, msg_type)
         if topic == "/localization/pose_estimator/exe_time_ms":
-            timestamp_header = int(msg.stamp.sec) + int(msg.stamp.nanosec) * 1e-9
+            timestamp_header = int(msg.stamp.sec) + \
+                int(msg.stamp.nanosec) * 1e-9
             ndt_exe_time_ms.append(
                 {
                     "timestamp": timestamp_header,
@@ -54,7 +57,8 @@ if __name__ == "__main__":
                 }
             )
         elif topic == "/localization/pose_estimator/pose":
-            timestamp_header = int(msg.header.stamp.sec) + int(msg.header.stamp.nanosec) * 1e-9
+            timestamp_header = int(msg.header.stamp.sec) + \
+                int(msg.header.stamp.nanosec) * 1e-9
             pose = msg.pose
             ndt_pose_list.append(
                 {
@@ -74,13 +78,18 @@ if __name__ == "__main__":
     df_ndt_exe_time_ms = pd.DataFrame(ndt_exe_time_ms)
     df_ndt_pose = pd.DataFrame(ndt_pose_list)
 
-    df_ndt_pose = interpolate_pose(df_ndt_pose, df_ndt_exe_time_ms["timestamp"])
+    df_ndt_pose = interpolate_pose(
+        df_ndt_pose, df_ndt_exe_time_ms["timestamp"])
 
     # rosbag path may be the path to the db3 file, or it may be the path to the directory containing it
     save_dir = (
         rosbag_path.parent if rosbag_path.is_dir() else rosbag_path.parent.parent
     ) / "exe_time_ms"
     save_dir.mkdir(exist_ok=True)
+
+    # save df
+    df_ndt_exe_time_ms.to_csv(
+        save_dir / "exe_time_ms.tsv", index=False, sep="\t", float_format="%.9f")
 
     # plot exe_time_ms
     plt.plot(df_ndt_exe_time_ms["timestamp"], df_ndt_exe_time_ms["value"])
