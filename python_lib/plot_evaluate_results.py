624f2f12d69e633a807dbb61610eaadb7f582424
Shintaro Sakoda <shintaro.sakoda@tier4.jp>
2024-02-28 10:31:42 +0900

    Updated plot_evaluate_results.py
    Signed-off-by: Shintaro Sakoda <shintaro.sakoda@tier4.jp>

--- python_lib/plot_evaluate_results.py
+++ python_lib/plot_evaluate_results.py
@@ -24,29 +24,60 @@ if __name__ == "__main__":
 
     result_dirs.sort()
 
-    suffix = "localization__pose_twist_fusion_filter__pose_result/relative_pose_summary.tsv"
+    suffix_error = "localization__pose_twist_fusion_filter__pose_result/relative_pose_summary.tsv"
+    suffix_time = "exe_time_ms/exe_time_ms.tsv"
 
     date_list = []
-    value_list = []
+    error_list = []
+    time_list = []
 
     for result_dir in result_dirs:
-        tsv_path = os.path.join(result_dir, suffix)
-        if not os.path.exists(tsv_path):
+        tsv_path_error = os.path.join(result_dir, suffix_error)
+        tsv_path_time = os.path.join(result_dir, suffix_time)
+        if not os.path.exists(tsv_path_error) or not os.path.exists(tsv_path_time):
             continue
-        df = pd.read_csv(tsv_path, sep="\t")
+
         date = os.path.basename(result_dir).split("_")
         date = date[0] + date[1]
         date = pd.to_datetime(date, format="%Y%m%d%H%M%S")
-        value = df['error_mean'].values[0]
         date_list.append(date)
-        value_list.append(value)
 
-    percentile_90 = np.percentile(value_list, 90)
-    plt.plot(date_list, value_list, marker=".")
-    save_path = os.path.join(target_dir, "error_mean.png")
-    plt.xlabel("date")
+        df_error = pd.read_csv(tsv_path_error, sep="\t")
+        error = df_error['error_mean'].values[0]
+        error_list.append(error)
+
+        df_time = pd.read_csv(tsv_path_time, sep="\t")
+        time = df_time['value'].mean()
+        time_list.append(time)
+
+    plt.figure(figsize=(6.4, 4.8 * 1.5))
+
+    # plot error
+    plt.subplot(2, 1, 1)
+    percentile_90 = np.percentile(error_list, 90)
+    plt.plot(date_list, error_list, marker=".", color="C0")
     plt.ylabel("error_mean")
+    plt.ylim(0, percentile_90 * 2)
+    plt.tick_params(
+        axis='x',
+        which='both',
+        bottom=False,
+        top=False,
+        labelbottom=False)
+    plt.grid(True)
+
+    # plot time
+    plt.subplot(2, 1, 2)
+    percentile_90 = np.percentile(time_list, 90)
+    plt.plot(date_list, time_list, marker=".", color="C1")
+    plt.xlabel("date")
+    plt.ylabel("exe_time_ms")
     plt.xticks(rotation=90)
     plt.ylim(0, percentile_90 * 2)
+    plt.grid(True)
+
+    plt.tight_layout()
+
+    save_path = os.path.join(target_dir, "error_mean.png")
     plt.savefig(save_path, bbox_inches='tight', pad_inches=0.05)
     print(f"save {save_path}")
