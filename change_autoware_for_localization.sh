9eeb4fceee66e5c519107409127ca543758af208
Shintaro Sakoda <sakoda@keio.jp>
2024-03-08 06:46:49 +0900

    Added change_autoware_for_localization.sh
    Signed-off-by: Shintaro Sakoda <sakoda@keio.jp>

---
+++ change_autoware_for_localization.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+set -eux
+
+# 現状のディレクトリがautowareまたはpilot-autoというプレフィックスを持つことを確認する
+current_dir=$(basename $(pwd))
+if [[ ! $current_dir =~ ^(autoware|pilot-auto) ]]; then
+    echo "This script must be run in a directory with a prefix of autoware or pilot-auto."
+    exit 1
+fi
+
+sed -i 's|input_traffic_light_topic_name" default="/perception/traffic_light_recognition/traffic_signals"|input_traffic_light_topic_name" default="/perception/traffic_light_recognition/traffic_signals_dummy"|' \
+  ./src/universe/autoware.universe/launch/tier4_planning_launch/launch/scenario_planning/lane_driving/behavior_planning/behavior_planning.launch.xml
