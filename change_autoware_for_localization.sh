ebd43b08e253da4648f7049549cf167fc056a751
Shintaro Sakoda <sakoda@keio.jp>
2024-03-09 12:56:53 +0900

    Updated change_autoware_for_localization.sh
    Signed-off-by: Shintaro Sakoda <sakoda@keio.jp>

--- change_autoware_for_localization.sh
+++ change_autoware_for_localization.sh
@@ -8,5 +8,8 @@ if [[ ! $current_dir =~ ^(autoware|pilot-auto) ]]; then
     exit 1
 fi
 
+# ./autoware/src/launcher/autoware_launch/autoware_launch/config/planning/preset/default_preset.yaml
+# ./autoware/src/launcher/autoware_launch/autoware_launch/config/planning/scenario_planning/common/common.param.yaml
+
 sed -i 's|input_traffic_light_topic_name" default="/perception/traffic_light_recognition/traffic_signals"|input_traffic_light_topic_name" default="/perception/traffic_light_recognition/traffic_signals_dummy"|' \
   ./src/universe/autoware.universe/launch/tier4_planning_launch/launch/scenario_planning/lane_driving/behavior_planning/behavior_planning.launch.xml
