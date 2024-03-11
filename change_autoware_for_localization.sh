1dd3c40270ec99c7805ad45fe8bb99696079b824
Shintaro Sakoda <shintaro.sakoda@tier4.jp>
2024-03-11 12:50:55 +0900

    Updated change_autoware_for_localization.sh
    Signed-off-by: Shintaro Sakoda <shintaro.sakoda@tier4.jp>

--- change_autoware_for_localization.sh
+++ change_autoware_for_localization.sh
@@ -8,8 +8,8 @@ if [[ ! $current_dir =~ ^(autoware|pilot-auto) ]]; then
     exit 1
 fi
 
-# ./autoware/src/launcher/autoware_launch/autoware_launch/config/planning/preset/default_preset.yaml
-# ./autoware/src/launcher/autoware_launch/autoware_launch/config/planning/scenario_planning/common/common.param.yaml
+sed -i '/launch_traffic_light_module/,/default/s/true/false/' \
+  ./src/launcher/autoware_launch/autoware_launch/config/planning/preset/default_preset.yaml
 
-sed -i 's|input_traffic_light_topic_name" default="/perception/traffic_light_recognition/traffic_signals"|input_traffic_light_topic_name" default="/perception/traffic_light_recognition/traffic_signals_dummy"|' \
-  ./src/universe/autoware.universe/launch/tier4_planning_launch/launch/scenario_planning/lane_driving/behavior_planning/behavior_planning.launch.xml
+sed -i 's/max_vel: 4.17/max_vel: 11.1/' \
+  ./src/launcher/autoware_launch/autoware_launch/config/planning/scenario_planning/common/common.param.yaml
