3bbf60fa64e0225fc943a728996133572164df7a
Shintaro Sakoda <shintaro.sakoda@tier4.jp>
2024-04-01 13:32:22 +0900

    Fixed extract_images_and_poses_from_rosbag.py
    Signed-off-by: Shintaro Sakoda <shintaro.sakoda@tier4.jp>

--- python_lib/extract_images_and_poses_from_rosbag.py
+++ python_lib/extract_images_and_poses_from_rosbag.py
@@ -15,6 +15,7 @@ import geometry_msgs
 from scipy.spatial.transform import Rotation, Slerp
 import matplotlib.pyplot as plt
 import rosbag2_py
+from interpolate_pose import interpolate_pose
 
 
 def save_camera_info_to_opencv_yaml(camera_info, filename):
@@ -36,53 +37,6 @@ def save_camera_info_to_opencv_yaml(camera_info, filename):
     fs.write("P", P)
 
     fs.release()
-
-
-def interpolate_pose_in_time(df: pd.DataFrame, target_timestamp_list: pd.Series) -> pd.DataFrame:
-    """ dfというデータフレームを、target_timestamp_listが示すタイムスタンプに合わせて補間する
-    制約)
-    * dfとtarget_timestamp_listはタイムスタンプでソートされていること
-    * dfはtarget_timestamp_listを覆うような前後に広い区間のタイムスタンプを持つこと
-    出力)
-    * dfと同じカラムを持ち、長さがtarget_timestamp_listと同じであるデータフレーム
-    """
-    POSITIONS_KEY = ['x', 'y', 'z']
-    ORIENTATIONS_KEY = ['qw', 'qx', 'qy', 'qz']
-    result_df = pd.DataFrame(columns=df.columns)
-    target_index = 0
-    df_index = 0
-    while df_index < len(df) - 1 and target_index < len(target_timestamp_list):
-        curr_time = df.iloc[df_index]['timestamp']
-        next_time = df.iloc[df_index + 1]['timestamp']
-        target_time = target_timestamp_list[target_index]
-
-        # target_timeを挟み込むようなdf_indexを探す
-        if not (curr_time <= target_time <= next_time):
-            df_index += 1
-            continue
-
-        curr_weight = (next_time - target_time) / (next_time - curr_time)
-        next_weight = 1.0 - curr_weight
-
-        curr_position = df.iloc[df_index][POSITIONS_KEY]
-        next_position = df.iloc[df_index + 1][POSITIONS_KEY]
-        target_position = curr_position * curr_weight + next_position * next_weight
-
-        curr_orientation = df.iloc[df_index][ORIENTATIONS_KEY]
-        next_orientation = df.iloc[df_index + 1][ORIENTATIONS_KEY]
-        curr_r = Rotation.from_quat(curr_orientation)
-        next_r = Rotation.from_quat(next_orientation)
-        slerp = Slerp([curr_time, next_time],
-                      Rotation.concatenate([curr_r, next_r]))
-        target_orientation = slerp([target_time]).as_quat()[0]
-
-        target_row = df.iloc[df_index].copy()
-        target_row['timestamp'] = target_timestamp_list[target_index]
-        target_row[POSITIONS_KEY] = target_position
-        target_row[ORIENTATIONS_KEY] = target_orientation
-        result_df = result_df.append(target_row)
-        target_index += 1
-    return result_df
 
 
 def create_reader(input_bag_dir: str, storage_id: str):
@@ -137,24 +91,40 @@ if __name__ == "__main__":
     parser.add_argument("output_dir", type=str)
     parser.add_argument("--storage_id", type=str,
                         default="sqlite3", choices=["mcap", "sqlite3"])
+    parser.add_argument("--image_topic_name", type=str, default="/sensing/camera/traffic_light/image_raw")
+    parser.add_argument("--camera_info_topic_name", type=str, default="/sensing/camera/traffic_light/camera_info")
+    parser.add_argument("--pose_topic_name", type=str, default="/localization/pose_twist_fusion_filter/biased_pose_with_covariance")
+    parser.add_argument("--image_interval", type=int, default=1)
     args = parser.parse_args()
 
     path_to_rosbag = args.path_to_rosbag
     output_dir = args.output_dir
     storage_id = args.storage_id
+    image_topic_name = args.image_topic_name
+    camera_info_topic_name = args.camera_info_topic_name
+    pose_topic_name = args.pose_topic_name
+    image_interval = args.image_interval
 
-    target_frame = "traffic_light_left_camera/camera_optical_link"
-    image_topic_name = "/sensing/camera/traffic_light/image_raw"
-    camera_info_topic_name = "/sensing/camera/traffic_light/camera_info"
-    pose_topic_name = "/localization/pose_twist_fusion_filter/biased_pose_with_covariance"
-    image_topic_type = Image()
+    image_topic_type = CompressedImage()
     pose_topic_type = PoseWithCovarianceStamped()
 
     reader, storage_options, converter_options = create_reader(
         path_to_rosbag, storage_id)
+
+    # save rosbag info
     os.makedirs(output_dir, exist_ok=True)
     with open(f"{output_dir}/rosbag_info.txt", "w") as f:
         f.write(f"{path_to_rosbag}\n")
+
+    # set filter
+    target_topics = [
+        image_topic_name,
+        camera_info_topic_name,
+        pose_topic_name,
+        "/tf_static",
+    ]
+    storage_filter = rosbag2_py.StorageFilter(topics=target_topics)
+    reader.set_filter(storage_filter)
 
     bridge = CvBridge()
     tf_buffer = Buffer()
@@ -163,6 +133,7 @@ if __name__ == "__main__":
     prev_image = None
     image_timestamp_list = list()
     image_list = list()
+    saved_camera_info = False
     columns = ["timestamp", "x", "y", "z", "qx", "qy", "qz", "qw"]
     df_pose = pd.DataFrame(columns=columns)
     while reader.has_next():
@@ -170,6 +141,7 @@ if __name__ == "__main__":
         t /= 1e9
         if topic == image_topic_name:
             image_msg = deserialize_message(data, image_topic_type)
+            camera_frame = image_msg.header.frame_id
             if image_topic_type == Image():
                 curr_image = bridge.imgmsg_to_cv2(
                     image_msg, desired_encoding="passthrough")
@@ -180,6 +152,8 @@ if __name__ == "__main__":
                 prev_image - curr_image).sum())
             prev_image = curr_image
             index_images_all += 1
+            if index_images_all % image_interval != 0:
+                continue
             if diff == 0:
                 continue
             image_timestamp_list.append(t)
@@ -189,7 +163,8 @@ if __name__ == "__main__":
             pose = pose_msg.pose.pose if pose_topic_type == PoseWithCovarianceStamped() else pose_msg.pose
             try:
                 pose = transform_pose_base_link_2_camera(
-                    pose, target_frame, tf_buffer, pose_msg.header.stamp)
+                    pose, camera_frame, tf_buffer, pose_msg.header.stamp
+                )
             except Exception as e:
                 print(e)
                 continue
@@ -204,19 +179,17 @@ if __name__ == "__main__":
                 pose.orientation.w,
             ]
         elif topic == camera_info_topic_name:
+            if saved_camera_info:
+                continue
             camera_info = deserialize_message(data, CameraInfo())
             save_camera_info_to_opencv_yaml(
                 camera_info, f"{output_dir}/camera_info.yaml")
-        elif topic == "/tf" or topic == "/tf_static":
-            is_static = (topic == "/tf_static")
+            saved_camera_info = True
+        elif topic == "/tf_static":
             tf_msg = deserialize_message(data, TFMessage())
             for transform_stamped in tf_msg.transforms:
-                if is_static:
-                    tf_buffer.set_transform_static(
-                        transform_stamped, "default_authority")
-                else:
-                    tf_buffer.set_transform(
-                        transform_stamped, "default_authority")
+                tf_buffer.set_transform_static(
+                    transform_stamped, "default_authority")
 
     image_timestamp_list = np.array(image_timestamp_list)
     image_list = np.array(image_list)
@@ -228,7 +201,7 @@ if __name__ == "__main__":
     image_timestamp_list = image_timestamp_list[ok_image_timestamp]
     image_list = image_list[ok_image_timestamp]
 
-    df_pose = interpolate_pose_in_time(df_pose, image_timestamp_list)
+    df_pose = interpolate_pose(df_pose, image_timestamp_list)
 
     os.makedirs(f"{output_dir}/images", exist_ok=True)
     for i, image in enumerate(image_list):
