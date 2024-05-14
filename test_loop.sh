543f4ea55f58f40e5dbeaea2a96fdae1757b57a5
Shintaro Sakoda <shintaro.sakoda@tier4.jp>
2024-05-14 10:02:28 +0900

    Added build before test
    Signed-off-by: Shintaro Sakoda <shintaro.sakoda@tier4.jp>

--- test_loop.sh
+++ test_loop.sh
@@ -4,6 +4,14 @@ set -eux
 TEST_TARGET_PKG=$1
 LOOP_NUM=${2:-100}
 
+colcon build --symlink-install \
+  --cmake-args \
+    -DCMAKE_BUILD_TYPE=Release \
+    -DBUILD_TESTING=ON \
+    -DCMAKE_CXX_FLAGS='-fprofile-arcs -ftest-coverage' \
+    -DCMAKE_C_FLAGS='-fprofile-arcs -ftest-coverage' \
+  --packages-select $TEST_TARGET_PKG
+
 for ((i=1; i<=LOOP_NUM; i++))
 do
   colcon test --packages-select ${TEST_TARGET_PKG} --event-handlers console_cohesion+ --return-code-on-test-failure
