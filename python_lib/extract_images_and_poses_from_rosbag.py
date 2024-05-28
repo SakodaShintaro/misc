a24b2401a002e4dfdee2b4a4579e324b4e8f0cb1
Shintaro Sakoda <shintaro.sakoda@tier4.jp>
2024-05-29 07:38:08 +0900

    Updated extract_images_and_poses.py
    Signed-off-by: Shintaro Sakoda <shintaro.sakoda@tier4.jp>

--- python_lib/extract_images_and_poses_from_rosbag.py
+++ python_lib/extract_images_and_poses_from_rosbag.py
@@ -1,39 +1,36 @@
 #!/usr/bin/env python3
 
 import argparse
-from cv_bridge import CvBridge
-from sensor_msgs.msg import Image, CompressedImage, CameraInfo
-from geometry_msgs.msg import Pose, PoseWithCovarianceStamped
 import numpy as np
 import cv2
-from rclpy.serialization import deserialize_message
 import os
 import pandas as pd
-from tf2_msgs.msg import TFMessage
 from tf2_ros import Buffer
-import geometry_msgs
 from scipy.spatial.transform import Rotation
 import matplotlib.pyplot as plt
 import rosbag2_py
 from interpolate_pose import interpolate_pose
+from parse_functions import parse_rosbag
+from builtin_interfaces.msg import Time
+from tqdm import tqdm
 
 
 def save_camera_info_to_opencv_yaml(camera_info, filename):
     fs = cv2.FileStorage(filename, cv2.FileStorage_WRITE)
 
-    fs.write("width", camera_info.width)
-    fs.write("height", camera_info.height)
+    fs.write("width", camera_info["width"])
+    fs.write("height", camera_info["height"])
 
-    D = np.array(camera_info.d, dtype=np.float32)
+    D = np.array(camera_info["D"], dtype=np.float32)
     fs.write("D", D)
 
-    K = np.array(camera_info.k, dtype=np.float32).reshape((3, 3))
+    K = np.array(camera_info["K"], dtype=np.float32).reshape((3, 3))
     fs.write("K", K)
 
-    R = np.array(camera_info.r, dtype=np.float32).reshape((3, 3))
+    R = np.array(camera_info["R"], dtype=np.float32).reshape((3, 3))
     fs.write("R", R)
 
-    P = np.array(camera_info.p, dtype=np.float32).reshape((3, 4))
+    P = np.array(camera_info["P"], dtype=np.float32).reshape((3, 4))
     fs.write("P", P)
 
     fs.release()
@@ -41,7 +38,8 @@ def save_camera_info_to_opencv_yaml(camera_info, filename):
 
 def create_reader(input_bag_dir: str, storage_id: str):
     storage_options = rosbag2_py.StorageOptions(
-        uri=input_bag_dir, storage_id=storage_id)
+        uri=input_bag_dir, storage_id=storage_id
+    )
     converter_options = rosbag2_py.ConverterOptions(
         input_serialization_format="cdr", output_serialization_format="cdr"
     )
@@ -50,50 +48,68 @@ def create_reader(input_bag_dir: str, storage_id: str):
     return reader, storage_options, converter_options
 
 
-def transform_pose_base_link_2_camera(pose: Pose, target_frame: str, tf_buffer, time):
-    # get static transform
-    transform = tf_buffer.lookup_transform(
-        target_frame="base_link",
-        source_frame=target_frame,
-        time=time)
-    # transform pose
-    R1: geometry_msgs.msg.Quaternion = transform.transform.rotation
-    R1: np.ndarray = Rotation.from_quat([R1.x, R1.y, R1.z, R1.w]).as_matrix()
-    t1: geometry_msgs.msg.Vector3 = transform.transform.translation
-    t1: np.ndarray = np.array([t1.x, t1.y, t1.z])
+def transform_pose_base_link_2_camera(df_pose: pd.DataFrame, transform):
+    # transform pose (camera_link to base_link)
+    R_c2b: np.ndarray = Rotation.from_quat(
+        [
+            transform.transform.rotation.x,
+            transform.transform.rotation.y,
+            transform.transform.rotation.z,
+            transform.transform.rotation.w,
+        ]
+    ).as_matrix()
+    t_c2b: np.ndarray = np.array(
+        [
+            transform.transform.translation.x,
+            transform.transform.translation.y,
+            transform.transform.translation.z,
+        ]
+    )
 
-    # pose
-    R2: geometry_msgs.msg.Quaternion = pose.orientation
-    R2: np.ndarray = Rotation.from_quat([R2.x, R2.y, R2.z, R2.w]).as_matrix()
-    t2: geometry_msgs.msg.Vector3 = pose.position
-    t2: np.ndarray = np.array([t2.x, t2.y, t2.z])
+    # pose (base_link to map)
+    R_b2m: np.ndarray = Rotation.from_quat(
+        df_pose[
+            ["orientation.x", "orientation.y", "orientation.z", "orientation.w"]
+        ].values,
+    ).as_matrix()
+    t_b2m: np.ndarray = df_pose[["position.x", "position.y", "position.z"]].values
 
     # transform
-    R: np.ndarray = np.dot(R2, R1)
-    t: np.ndarray = np.dot(R2, t1) + t2
-    q: np.ndarray = Rotation.from_matrix(R).as_quat()
+    R_c2m: np.ndarray = np.dot(R_b2m, R_c2b)
+    t_c2m: np.ndarray = np.dot(R_b2m, t_c2b) + t_b2m
+    q_c2m: np.ndarray = Rotation.from_matrix(R_c2m).as_quat()
 
-    # convert to geometry_msgs.msg.Pose
-    result_pose = Pose()
-    result_pose.orientation.x = q[0]
-    result_pose.orientation.y = q[1]
-    result_pose.orientation.z = q[2]
-    result_pose.orientation.w = q[3]
-    result_pose.position.x = t[0]
-    result_pose.position.y = t[1]
-    result_pose.position.z = t[2]
-    return result_pose
+    df_pose["orientation.x"] = q_c2m[:, 0]
+    df_pose["orientation.y"] = q_c2m[:, 1]
+    df_pose["orientation.z"] = q_c2m[:, 2]
+    df_pose["orientation.w"] = q_c2m[:, 3]
+    df_pose["position.x"] = t_c2m[:, 0]
+    df_pose["position.y"] = t_c2m[:, 1]
+    df_pose["position.z"] = t_c2m[:, 2]
 
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
     parser.add_argument("path_to_rosbag", type=str)
     parser.add_argument("output_dir", type=str)
-    parser.add_argument("--storage_id", type=str,
-                        default="sqlite3", choices=["mcap", "sqlite3"])
-    parser.add_argument("--image_topic_name", type=str, default="/sensing/camera/traffic_light/image_raw")
-    parser.add_argument("--camera_info_topic_name", type=str, default="/sensing/camera/traffic_light/camera_info")
-    parser.add_argument("--pose_topic_name", type=str, default="/localization/pose_twist_fusion_filter/biased_pose_with_covariance")
+    parser.add_argument(
+        "--storage_id", type=str, default="sqlite3", choices=["mcap", "sqlite3"]
+    )
+    parser.add_argument(
+        "--image_topic_name",
+        type=str,
+        default="/sensing/camera/traffic_light/image_raw",
+    )
+    parser.add_argument(
+        "--camera_info_topic_name",
+        type=str,
+        default="/sensing/camera/traffic_light/camera_info",
+    )
+    parser.add_argument(
+        "--pose_topic_name",
+        type=str,
+        default="/localization/pose_twist_fusion_filter/biased_pose_with_covariance",
+    )
     parser.add_argument("--image_interval", type=int, default=1)
     args = parser.parse_args()
 
@@ -105,120 +121,68 @@ if __name__ == "__main__":
     pose_topic_name = args.pose_topic_name
     image_interval = args.image_interval
 
-    image_topic_type = CompressedImage()
-    pose_topic_type = PoseWithCovarianceStamped()
-
-    reader, storage_options, converter_options = create_reader(
-        path_to_rosbag, storage_id)
-
-    # save rosbag info
-    os.makedirs(output_dir, exist_ok=True)
-    with open(f"{output_dir}/rosbag_info.txt", "w") as f:
-        f.write(f"{path_to_rosbag}\n")
-
-    # set filter
     target_topics = [
         image_topic_name,
         camera_info_topic_name,
         pose_topic_name,
         "/tf_static",
     ]
-    storage_filter = rosbag2_py.StorageFilter(topics=target_topics)
-    reader.set_filter(storage_filter)
 
-    bridge = CvBridge()
+    df_dict = parse_rosbag(path_to_rosbag, target_topics)
+
+    # save rosbag info
+    os.makedirs(output_dir, exist_ok=True)
+    with open(f"{output_dir}/rosbag_info.txt", "w") as f:
+        f.write(f"{path_to_rosbag}\n")
+
+    # save camera_info.yaml
+    save_camera_info_to_opencv_yaml(
+        df_dict[camera_info_topic_name].iloc[0], f"{output_dir}/camera_info.yaml"
+    )
+
+    df_image = df_dict[image_topic_name]
+    df_pose = df_dict[pose_topic_name]
+
+    camera_frame = df_image["frame_id"].values[0]
+    print(f"{camera_frame=}")
+
+    # transformを取得
+    df_tf_static = df_dict["/tf_static"]
     tf_buffer = Buffer()
+    for _, row in df_tf_static.iterrows():
+        for transform_stamped in row["transforms"]:
+            tf_buffer.set_transform_static(transform_stamped, "default_authority")
+    transform = tf_buffer.lookup_transform(
+        target_frame="base_link", source_frame=camera_frame, time=Time()
+    )
 
-    index_images_all = 0
-    prev_image = None
-    image_timestamp_list = list()
-    image_list = list()
-    saved_camera_info = False
-    columns = [
-        "timestamp",
-        "position.x",
-        "position.y",
-        "position.z",
-        "orientation.x",
-        "orientation.y",
-        "orientation.z",
-        "orientation.w",
-    ]
-    df_pose = pd.DataFrame(columns=columns)
-    while reader.has_next():
-        (topic, data, t) = reader.read_next()
-        t /= 1e9
-        if topic == image_topic_name:
-            image_msg = deserialize_message(data, image_topic_type)
-            camera_frame = image_msg.header.frame_id
-            if image_topic_type == Image():
-                curr_image = bridge.imgmsg_to_cv2(
-                    image_msg, desired_encoding="passthrough")
-            elif image_topic_type == CompressedImage():
-                curr_image = bridge.compressed_imgmsg_to_cv2(
-                    image_msg, desired_encoding="passthrough")
-            diff = (1 if prev_image is None else np.abs(
-                prev_image - curr_image).sum())
-            prev_image = curr_image
-            index_images_all += 1
-            if index_images_all % image_interval != 0:
-                continue
-            if diff == 0:
-                continue
-            image_timestamp_list.append(t)
-            image_list.append(curr_image)
-        elif topic == pose_topic_name:
-            pose_msg = deserialize_message(data, pose_topic_type)
-            pose = pose_msg.pose.pose if pose_topic_type == PoseWithCovarianceStamped() else pose_msg.pose
-            try:
-                pose = transform_pose_base_link_2_camera(
-                    pose, camera_frame, tf_buffer, pose_msg.header.stamp
-                )
-            except Exception as e:
-                print(e)
-                continue
-            df_pose.loc[len(df_pose)] = [
-                t,
-                pose.position.x,
-                pose.position.y,
-                pose.position.z,
-                pose.orientation.x,
-                pose.orientation.y,
-                pose.orientation.z,
-                pose.orientation.w,
-            ]
-        elif topic == camera_info_topic_name:
-            if saved_camera_info:
-                continue
-            camera_info = deserialize_message(data, CameraInfo())
-            save_camera_info_to_opencv_yaml(
-                camera_info, f"{output_dir}/camera_info.yaml")
-            saved_camera_info = True
-        elif topic == "/tf_static":
-            tf_msg = deserialize_message(data, TFMessage())
-            for transform_stamped in tf_msg.transforms:
-                tf_buffer.set_transform_static(
-                    transform_stamped, "default_authority")
+    # df_poseを変換する
+    # df_pose : base_link in map
+    # 欲しいpose : camera_frame in map
+    # tf_staticでcamera_frame in base_linkを取得してdf_poseに右からかける
+    transform_pose_base_link_2_camera(df_pose, transform)
 
-    image_timestamp_list = np.array(image_timestamp_list)
-    image_list = np.array(image_list)
+    image_timestamp_list = df_image["timestamp"].values
+    image_list = df_image["image"].values
 
     min_pose_t = df_pose["timestamp"].min()
     max_pose_t = df_pose["timestamp"].max()
-    ok_image_timestamp = (min_pose_t < image_timestamp_list) * \
-        (image_timestamp_list < max_pose_t)
+    ok_image_timestamp = (min_pose_t < image_timestamp_list) * (
+        image_timestamp_list < max_pose_t
+    )
     image_timestamp_list = image_timestamp_list[ok_image_timestamp]
     image_list = image_list[ok_image_timestamp]
 
     df_pose = interpolate_pose(df_pose, image_timestamp_list)
 
     os.makedirs(f"{output_dir}/images", exist_ok=True)
+    bar = tqdm(total=len(image_list))
     for i, image in enumerate(image_list):
         save_path = f"{output_dir}/images/{i:08d}.png"
         cv2.imwrite(save_path, image)
+        bar.update(1)
 
-    df_pose.to_csv(f"{output_dir}/pose.tsv",
-                   index=True, sep="\t", float_format="%.12f")
+    df_pose.to_csv(f"{output_dir}/pose.tsv", index=True, sep="\t", float_format="%.12f")
 
     # plot all of trajectory
     save_path = f"{output_dir}/plot_pose.png"
