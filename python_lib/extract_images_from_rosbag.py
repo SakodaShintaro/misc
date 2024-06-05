d9d37d3ea7cf99182ac9f9ae359b5657f79a19a7
Shintaro Sakoda <shintaro.sakoda@tier4.jp>
2024-06-05 14:31:31 +0900

    Fixed extract_images_from_rosbag.py
    Signed-off-by: Shintaro Sakoda <shintaro.sakoda@tier4.jp>

--- python_lib/extract_images_from_rosbag.py
+++ python_lib/extract_images_from_rosbag.py
@@ -2,17 +2,30 @@
 
 import argparse
 from cv_bridge import CvBridge
-from sensor_msgs.msg import Image
+from sensor_msgs.msg import Image, CameraInfo
 import cv2
 from rclpy.serialization import deserialize_message
+from rosidl_runtime_py.utilities import get_message
 import os
 import rosbag2_py
 import pandas as pd
+from parse_functions import parse_stamp, parse_CameraInfo
+from collections import defaultdict
+
+
+def parse_args():
+    parser = argparse.ArgumentParser()
+    parser.add_argument("path_to_rosbag", type=str)
+    parser.add_argument("output_dir", type=str)
+    parser.add_argument("--use_cvt_color", action="store_true")
+    parser.add_argument("--use_timestamp_as_filename", action="store_true")
+    return parser.parse_args()
 
 
 def create_reader(input_bag_dir: str, storage_id: str):
     storage_options = rosbag2_py.StorageOptions(
-        uri=input_bag_dir, storage_id=storage_id)
+        uri=input_bag_dir, storage_id=storage_id
+    )
     converter_options = rosbag2_py.ConverterOptions(
         input_serialization_format="cdr", output_serialization_format="cdr"
     )
@@ -22,44 +35,78 @@ def create_reader(input_bag_dir: str, storage_id: str):
 
 
 if __name__ == "__main__":
-    parser = argparse.ArgumentParser()
-    parser.add_argument("path_to_rosbag", type=str)
-    parser.add_argument("output_dir", type=str)
-    parser.add_argument("--topic_name", type=str,
-                        default="/sensing/camera/traffic_light/image_raw")
-    parser.add_argument("--use_cvt_color", action="store_true")
-    args = parser.parse_args()
-
+    args = parse_args()
     path_to_rosbag = args.path_to_rosbag
     output_dir = args.output_dir
-    topic_name = args.topic_name
     use_cvt_color = args.use_cvt_color
 
     reader, storage_options, converter_options = create_reader(
-        path_to_rosbag, "sqlite3")
+        path_to_rosbag, "sqlite3"
+    )
+    topic_types = reader.get_all_topics_and_types()
+    type_map = {
+        topic_types[i].name: topic_types[i].type for i in range(len(topic_types))
+    }
+
+    camera_topic_list = [
+        topic.name for topic in topic_types if topic.name.startswith("/sensing/camera")
+    ]
+
     os.makedirs(output_dir, exist_ok=True)
 
     bridge = CvBridge()
 
     index_images = 0
-    timestamp_list = []
+
+    camera_info_df_dict = defaultdict(list)
+    timestamp_df_dict = defaultdict(list)
+
     while reader.has_next():
         (topic, data, t) = reader.read_next()
-        if topic != topic_name:
+        if topic not in camera_topic_list:
             continue
 
-        image_msg = deserialize_message(data, Image())
-        cv_image = bridge.imgmsg_to_cv2(
-            image_msg, desired_encoding="passthrough")
+        elements = topic.split("/")
+        camera_name = elements[3]
+
+        msg_type = get_message(type_map[topic])
+
+        if msg_type == CameraInfo:
+            msg = deserialize_message(data, msg_type)
+            camera_info = parse_CameraInfo(msg)
+            camera_info_df_dict[camera_name].append(camera_info)
+            continue
+
+        image_msg = deserialize_message(data, msg_type)
+        timestamp_header = parse_stamp(image_msg.header.stamp)
+        cv_image = (
+            bridge.imgmsg_to_cv2(image_msg, desired_encoding="passthrough")
+            if msg_type == Image
+            else bridge.compressed_imgmsg_to_cv2(image_msg)
+        )
         if use_cvt_color:
             cv_image = cv2.cvtColor(cv_image, cv2.COLOR_BGR2RGB)
-        os.makedirs(f"{output_dir}/images", exist_ok=True)
-        save_path = f"{output_dir}/images/{index_images:08d}.png"
+
+        save_dir = f"{output_dir}/{camera_name}"
+        os.makedirs(save_dir, exist_ok=True)
+        save_path = (
+            f"{save_dir}/{timestamp_header:018d}.png"
+            if args.use_timestamp_as_filename
+            else f"{save_dir}/{index_images:08d}.png"
+        )
         cv2.imwrite(save_path, cv_image)
-        timestamp_list.append(t)
+        timestamp_df_dict[camera_name].append(timestamp_header)
         print(f"\rtimestamp = {t}", end="")
         index_images += 1
 
     print()
-    df_timestamp = pd.DataFrame(timestamp_list, columns=["timestamp"])
-    df_timestamp.to_csv(f"{output_dir}/image_timestamps.tsv", index=False)
+    for camera_name, df in timestamp_df_dict.items():
+        df = pd.DataFrame(df)
+        df.columns = ["timestamp"]
+        df.to_csv(f"{output_dir}/timestamps_{camera_name}.tsv", index=False)
+        print(f"{camera_name} timestamps: {len(df)} msgs")
+
+    for camera_name, df in camera_info_df_dict.items():
+        df = pd.DataFrame(df)
+        df.to_csv(f"{output_dir}/camera_info_{camera_name}.tsv", index=False)
+        print(f"{camera_name} camera_info: {len(df)} msgs")
