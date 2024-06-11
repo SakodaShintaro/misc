fe2f88ae1ba773cc7afc0393af465de65bac92c3
Shintaro Sakoda <shintaro.sakoda@tier4.jp>
2024-06-11 10:23:58 +0900

    Filter clock
    Signed-off-by: Shintaro Sakoda <shintaro.sakoda@tier4.jp>

--- list_single_published_topics.sh
+++ list_single_published_topics.sh
@@ -3,6 +3,11 @@
 TOPIC_LIST=$(ros2 topic list)
 
 for topic in $TOPIC_LIST; do
+  # /clockは除外
+  if [[ $topic == "/clock" ]]; then
+    continue
+  fi
+
   msg=$(ros2 topic info -v $topic)
   # msgに
   #   Publisher count: 1
