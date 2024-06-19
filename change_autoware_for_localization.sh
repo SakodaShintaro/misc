635db6728b49df062f5dcc67726f21ddb4e3297d
Shintaro Sakoda <shintaro.sakoda@tier4.jp>
2024-06-19 09:26:20 +0900

    Changed max_vel
    Signed-off-by: Shintaro Sakoda <shintaro.sakoda@tier4.jp>

--- change_autoware_for_localization.sh
+++ change_autoware_for_localization.sh
@@ -11,5 +11,5 @@ fi
 sed -i '/launch_traffic_light_module/,/default/s/true/false/' \
   ./src/launcher/autoware_launch/autoware_launch/config/planning/preset/default_preset.yaml
 
-sed -i 's/max_vel: 4.17/max_vel: 11.1/' \
+sed -i 's/max_vel: 4.17/max_vel: 22.2/' \
   ./src/launcher/autoware_launch/autoware_launch/config/planning/scenario_planning/common/common.param.yaml
