835013a7ccc3ebd23371d1ab40c67993f5248481
Shintaro Sakoda <shintaro.sakoda@tier4.jp>
2024-07-26 18:14:25 +0900

    Added calc_imu_bias.py
    Signed-off-by: Shintaro Sakoda <shintaro.sakoda@tier4.jp>

---
+++ python_lib/calc_imu_bias.py
@@ -0,0 +1,75 @@
+"""(静止状態であるrosbagと前提して)IMUのバイアスを計算するスクリプト"""
+
+import argparse
+import matplotlib.pyplot as plt
+from pathlib import Path
+from parse_functions import parse_rosbag
+
+
+def parse_args() -> argparse.Namespace:
+    parser = argparse.ArgumentParser()
+    parser.add_argument("rosbag_path", type=Path)
+    return parser.parse_args()
+
+
+if __name__ == "__main__":
+    args = parse_args()
+    rosbag_path = args.rosbag_path
+
+    target_topic = "/sensing/imu/imu_data"
+
+    df_dict = parse_rosbag(str(rosbag_path), [target_topic])
+
+    # rosbag path may be the path to the db3 file, or it may be the path to the directory containing it
+    save_dir = (
+        rosbag_path.parent if rosbag_path.is_dir() else rosbag_path.parent.parent
+    ) / "imu_data"
+    save_dir.mkdir(exist_ok=True)
+
+    # save as csv
+    for topic_name in [target_topic]:
+        df = df_dict[topic_name]
+        if len(df) == 0:
+            print(f"!{topic_name} is empty")
+            continue
+        filename = topic_name.replace("/sensing/", "").replace("/", "_")
+        df.to_csv(
+            save_dir / f"{filename}.tsv",
+            index=False,
+            sep="\t",
+            float_format="%.9f",
+        )
+
+    df = df_dict[target_topic]
+
+    """
+    print(df.head())
+                timestamp           frame_id  angular_velocity.x  angular_velocity.y  angular_velocity.z  linear_acceleration.x  linear_acceleration.y  linear_acceleration.z  orientation.x  orientation.y  orientation.z  orientation.w
+    0  1721370795397252788  tamagawa/imu_link            0.000426           -0.002344            0.009800               0.061035              -0.140381              -9.713745            0.0            0.0            0.0            1.0
+    1  1721370795402274208  tamagawa/imu_link           -0.000107           -0.002024            0.008203               0.067139              -0.106812              -9.844971            0.0            0.0            0.0            1.0
+    2  1721370795407268850  tamagawa/imu_link           -0.001917           -0.002663            0.007883               0.027466              -0.131226              -9.698486            0.0            0.0            0.0            1.0
+    3  1721370795412342401  tamagawa/imu_link           -0.001278           -0.001278            0.007776               0.042725              -0.085449              -9.796143            0.0            0.0            0.0            1.0
+    4  1721370795417306159  tamagawa/imu_link           -0.000639           -0.002131            0.008629               0.070190              -0.119019              -9.768677            0.0            0.0            0.0            1.0
+    """
+
+    print(f"{df['angular_velocity.x'].mean()=:+.6f}")
+    print(f"{df['angular_velocity.y'].mean()=:+.6f}")
+    print(f"{df['angular_velocity.z'].mean()=:+.6f}")
+
+    # smoothing
+    df["angular_velocity.x"] = df["angular_velocity.x"].rolling(window=100).mean()
+    df["angular_velocity.y"] = df["angular_velocity.y"].rolling(window=100).mean()
+    df["angular_velocity.z"] = df["angular_velocity.z"].rolling(window=100).mean()
+
+    # plot
+    plt.plot(df["timestamp"], df["angular_velocity.x"], label="angular_velocity.x")
+    plt.plot(df["timestamp"], df["angular_velocity.y"], label="angular_velocity.y")
+    plt.plot(df["timestamp"], df["angular_velocity.z"], label="angular_velocity.z")
+    plt.legend()
+    plt.grid()
+    plt.title(target_topic)
+    plt.xlabel("timestamp")
+    plt.ylabel("angular_velocity[rad/s]")
+    save_path = save_dir / "angular_velocity.png"
+    plt.savefig(save_path, bbox_inches="tight", pad_inches=0.05)
+    print(f"Save {save_path}")
