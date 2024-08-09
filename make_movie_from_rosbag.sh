5983bc1ca99f73d7bb25c656bd11dd027cf30a9a
Shintaro Sakoda <shintaro.sakoda@tier4.jp>
2024-08-09 10:47:07 +0900

    make_movie_from_rosbag.sh 作りかけ
    Signed-off-by: Shintaro Sakoda <shintaro.sakoda@tier4.jp>

---
+++ make_movie_from_rosbag.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+set -eux
+
+TARGET_ROSBAG=$(readlink -f $1)
+TARGET_TOPIC=${2:-camera0}
+
+# 読み込み
+set +eux
+source $HOME/autoware/install/setup.bash
+set -eux
+
+python3 ~/misc/python_lib/extract_images_from_rosbag.py \
+  $TARGET_ROSBAG \
+  $TARGET_ROSBAG/../images \
+  --target_camera_name=$TARGET_TOPIC \
+  --skip_num=5
+
+# python3 ~/misc/python_lib/extract_poses_from_rosbag.py
+# python3 ~/misc/python_lib/calc_camera_pose.py
+# python3 ~/misc/python_lib/plot_camera_and_pose.py
+# ~/misc/ffmpeg_lib/make_mp4_from_unsequential_png.sh
