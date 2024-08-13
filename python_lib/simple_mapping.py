386294cf05801b2e27f36e9dc2bc6b6d5088e974
Shintaro Sakoda <shintaro.sakoda@tier4.jp>
2024-08-13 17:23:06 +0900

    Fixed mapping
    Signed-off-by: Shintaro Sakoda <shintaro.sakoda@tier4.jp>

--- python_lib/simple_mapping.py
+++ python_lib/simple_mapping.py
@@ -10,7 +10,7 @@ from rclpy.serialization import deserialize_message
 from rosidl_runtime_py.utilities import get_message
 from sensor_msgs_py.point_cloud2 import read_points
 import numpy as np
-from scipy.spatial.transform import Rotation, Slerp
+from scipy.spatial.transform import Rotation
 import open3d as o3d
 from tqdm import tqdm
 
@@ -20,18 +20,15 @@ def parse_args():
     parser.add_argument("rosbag_path", type=Path)
     parser.add_argument("points_topic", type=str)
     parser.add_argument("pose_tsv", type=Path)
+    parser.add_argument("--save_npy", action="store_true")
     return parser.parse_args()
 
 
-if __name__ == "__main__":
+def save_npy(args):
     args = parse_args()
     rosbag_path = args.rosbag_path
     points_topic = args.points_topic
-    pose_tsv = args.pose_tsv
-
-    # poseを取得
-    df_pose = pd.read_csv(pose_tsv, delimiter="\t")
-    print(df_pose.head())
+    _ = args.pose_tsv
 
     serialization_format = "cdr"
     storage_options = rosbag2_py.StorageOptions(
@@ -52,10 +49,9 @@ if __name__ == "__main__":
 
     storage_filter = rosbag2_py.StorageFilter(topics=[points_topic])
     reader.set_filter(storage_filter)
-    xyz_all = []
 
-    pose_index = 0
-    progress = tqdm(total=len(df_pose))
+    save_dir = rosbag_path.parent / "sensor_pcd"
+    save_dir.mkdir(exist_ok=True, parents=True)
 
     while reader.has_next():
         (topic, data, timestamp_rosbag) = reader.read_next()
@@ -63,24 +59,46 @@ if __name__ == "__main__":
         msg = deserialize_message(data, msg_type)
         points = read_points(msg)
         xyz = np.array([(p["x"], p["y"], p["z"]) for p in points], dtype=np.float32)
-        xyz = xyz[::100]
         timestamp_header = parse_stamp(msg.header.stamp)
-        while (
-            pose_index < len(df_pose)
-            and df_pose.iloc[pose_index]["timestamp"] < timestamp_header
-        ):
-            pose_index += 1
-            progress.update(1)
-        if pose_index >= len(df_pose):
-            break
-        curr_pose = df_pose.iloc[pose_index]
+        save_path = save_dir / f"{timestamp_header}.npy"
+        np.save(save_path, xyz)
 
-        t = curr_pose[["position.x", "position.y", "position.z"]].values
-        q = curr_pose[
-            ["orientation.x", "orientation.y", "orientation.z", "orientation.w"]
-        ].values
-        r = Rotation.from_quat(q)
-        xyz = (r.apply(xyz) + t).astype(np.float32)
+
+def mapping(args):
+    args = parse_args()
+    rosbag_path = args.rosbag_path
+    _ = args.points_topic
+    pose_tsv = args.pose_tsv
+    df_pose = pd.read_csv(pose_tsv, delimiter="\t")
+
+    save_dir = rosbag_path.parent / "sensor_pcd"
+    file_list = sorted(list(save_dir.glob("*.npy")))
+    timestamp_list = [int(f.stem) for f in file_list]
+    timestamp_list = pd.Series(timestamp_list)
+    cond = (df_pose.iloc[0]["timestamp"] <= timestamp_list) & (
+        timestamp_list <= df_pose.iloc[-1]["timestamp"]
+    )
+    timestamp_list = timestamp_list[cond]
+
+    df_interpolated_pose = interpolate_pose(df_pose, timestamp_list)
+
+    print(len(timestamp_list), len(df_interpolated_pose))
+
+    xyz_all = []
+    for i, timestamp in enumerate(tqdm(timestamp_list)):
+        file_path = save_dir / f"{timestamp}.npy"
+        xyz = np.load(file_path)
+        xyz = xyz[::100]
+        pose = df_interpolated_pose.iloc[i]
+        r = Rotation.from_quat(
+            [
+                pose["orientation.x"],
+                pose["orientation.y"],
+                pose["orientation.z"],
+                pose["orientation.w"],
+            ]
+        )
+        xyz = r.apply(xyz) + pose[["position.x", "position.y", "position.z"]].values
         xyz_all.append(xyz)
 
     # open3dで可視化
@@ -88,3 +106,14 @@ if __name__ == "__main__":
     pcd.points = o3d.utility.Vector3dVector(np.vstack(xyz_all))
     o3d.visualization.draw_geometries([pcd])
 
+    save_path = save_dir.parent / "mapped.npy"
+    np.save(save_path, np.vstack(xyz_all))
+
+
+if __name__ == "__main__":
+    args = parse_args()
+
+    if args.save_npy:
+        save_npy(args)
+
+    mapping(args)
