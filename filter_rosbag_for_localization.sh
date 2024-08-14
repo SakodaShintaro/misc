16e44f144ceb16b14e2e5a2187f024abc4df9f58
Shintaro Sakoda <shintaro.sakoda@tier4.jp>
2024-08-14 17:40:56 +0900

    Removed topics
    Signed-off-by: Shintaro Sakoda <shintaro.sakoda@tier4.jp>

--- filter_rosbag_for_localization.sh
+++ filter_rosbag_for_localization.sh
@@ -20,12 +20,10 @@ TARGET=$(readlink -f $1)
 ros2 bag filter ${TARGET} -o ${TARGET}_filtered --include \
     "/sensing/camera/traffic_light/camera_info" \
     "/sensing/camera/traffic_light/image_raw" \
-    "/sensing/gnss/pose" \
     "/sensing/gnss/pose_with_covariance" \
     "/sensing/imu/tamagawa/imu_raw" \
     "/sensing/lidar/top/pointcloud_raw" \
     "/sensing/lidar/top/pointcloud_raw_ex" \
     "/vehicle/status/velocity_status" \
     "/awsim/ground_truth/localization/kinematic_state" \
-    "/awsim/ground_truth/vehicle/pose" \
     "/initialpose"
