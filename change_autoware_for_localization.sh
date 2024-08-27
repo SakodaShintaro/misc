6d11c44bc60871df6250614fe6c408cb4641ef25
Shintaro Sakoda <shintaro.sakoda@tier4.jp>
2024-08-27 12:57:09 +0900

    Added awsim_sensor_kit_launch merge
    Signed-off-by: Shintaro Sakoda <shintaro.sakoda@tier4.jp>

--- change_autoware_for_localization.sh
+++ change_autoware_for_localization.sh
@@ -13,3 +13,6 @@ sed -i '/launch_traffic_light_module/,/default/s/true/false/' \
 
 sed -i 's/max_vel: 4.17/max_vel: 22.2/' \
   ./src/launcher/autoware_launch/autoware_launch/config/planning/scenario_planning/common/common.param.yaml
+
+cd ./src/sensor_kit/external/awsim_sensor_kit_launch/
+~/misc/merge_from_url.sh https://github.com/knzo25/awsim_sensor_kit_launch/tree/chore/imu_corrector_refactor
