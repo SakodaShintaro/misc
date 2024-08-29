b2936cdf27b9e753ec5032bed40e1fc160e54966
Shintaro Sakoda <shintaro.sakoda@tier4.jp>
2024-08-29 13:36:37 +0900

    Modified compare_two_dlr_result.py
    Signed-off-by: Shintaro Sakoda <shintaro.sakoda@tier4.jp>

--- python_lib/compare_two_dlr_result.py
+++ python_lib/compare_two_dlr_result.py
@@ -32,7 +32,8 @@ if __name__ == "__main__":
     set1 = {d.name for d in dir_list1}
     set2 = {d.name for d in dir_list2}
     common_dir_list = sorted(set1 & set2)
-    print(common_dir_list)
+    # "LM_regression_"を含むディレクトリ名を抽出
+    common_dir_list = [s for s in common_dir_list if "LM_regression_" in s]
 
     target_path = (
         "compare_trajectories/localization__kinematic_state_result/relative_pose_summary.tsv"
