49282f293412eed081dfa092a328502670c1ec2b
Shintaro Sakoda <shintaro.sakoda@tier4.jp>
2024-09-10 13:46:41 +0900

    Added date
    Signed-off-by: Shintaro Sakoda <shintaro.sakoda@tier4.jp>

--- update_autoware.sh
+++ update_autoware.sh
@@ -10,7 +10,7 @@ vcs import src < autoware.repos
 vcs import src < simulator.repos
 vcs import src < tools.repos
 vcs pull src
-vcs export src --exact > my_autoware.repos
+vcs export src --exact > my_autoware_$(date +"%Y%m%d").repos
 rosdep update
 rosdep install -y --from-paths src --ignore-src --rosdistro $ROS_DISTRO
 $(dirname $0)/build_with_custom_jobs.sh ${JOB_COUNT}
