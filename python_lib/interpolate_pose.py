1922c51bd6955f9051ae204c60515025ba5e177e
Shintaro Sakoda <shintaro.sakoda@tier4.jp>
2024-09-13 13:41:22 +0900

    Applied formatter
    Signed-off-by: Shintaro Sakoda <shintaro.sakoda@tier4.jp>

--- python_lib/interpolate_pose.py
+++ python_lib/interpolate_pose.py
@@ -13,7 +13,7 @@ def interpolate_pose(
         "orientation.z",
     ],
 ) -> pd.DataFrame:
-    """ Interpolate each pose in df_pose to match the timestamp in target_timestamp
+    """Interpolate each pose in df_pose to match the timestamp in target_timestamp
     Constraints)
     * df_pose and target_timestamp must be sorted by timestamp
     * df_pose must have timestamps with a larger interval than target_timestamp
@@ -22,10 +22,10 @@ def interpolate_pose(
     出力)
     * DataFrame with same columns as df_pose and length same as target_timestamp
     """
-    assert df_pose['timestamp'].is_monotonic_increasing
+    assert df_pose["timestamp"].is_monotonic_increasing
     assert target_timestamp.is_monotonic_increasing
-    assert df_pose.iloc[0]['timestamp'] <= target_timestamp.iloc[0]
-    assert target_timestamp.iloc[-1] <= df_pose.iloc[-1]['timestamp']
+    assert df_pose.iloc[0]["timestamp"] <= target_timestamp.iloc[0]
+    assert target_timestamp.iloc[-1] <= df_pose.iloc[-1]["timestamp"]
     assert len(df_pose) > 0, f"{len(df_pose)=}"
     assert len(target_timestamp) > 0, f"{len(target_timestamp)=}"
 
@@ -35,13 +35,13 @@ def interpolate_pose(
     target_index = 0
     df_index = 0
     data_dict = {
-        'timestamp': [],
+        "timestamp": [],
         **{key: [] for key in POSITIONS_KEY},
         **{key: [] for key in ORIENTATIONS_KEY},
     }
     while df_index < len(df_pose) - 1 and target_index < len(target_timestamp):
-        curr_time = df_pose.iloc[df_index]['timestamp']
-        next_time = df_pose.iloc[df_index + 1]['timestamp']
+        curr_time = df_pose.iloc[df_index]["timestamp"]
+        next_time = df_pose.iloc[df_index + 1]["timestamp"]
         target_time = target_timestamp[target_index]
 
         # Find a df_index that includes target_time
@@ -60,11 +60,10 @@ def interpolate_pose(
         next_orientation = df_pose.iloc[df_index + 1][ORIENTATIONS_KEY]
         curr_r = Rotation.from_quat(curr_orientation)
         next_r = Rotation.from_quat(next_orientation)
-        slerp = Slerp([curr_time, next_time],
-                      Rotation.concatenate([curr_r, next_r]))
+        slerp = Slerp([curr_time, next_time], Rotation.concatenate([curr_r, next_r]))
         target_orientation = slerp([target_time]).as_quat()[0]
 
-        data_dict['timestamp'].append(target_time)
+        data_dict["timestamp"].append(target_time)
         data_dict[POSITIONS_KEY[0]].append(target_position[0])
         data_dict[POSITIONS_KEY[1]].append(target_position[1])
         data_dict[POSITIONS_KEY[2]].append(target_position[2])
@@ -73,5 +72,4 @@ def interpolate_pose(
         data_dict[ORIENTATIONS_KEY[2]].append(target_orientation[2])
         data_dict[ORIENTATIONS_KEY[3]].append(target_orientation[3])
         target_index += 1
-    result_df = pd.DataFrame(data_dict)
-    return result_df
+    return pd.DataFrame(data_dict)
