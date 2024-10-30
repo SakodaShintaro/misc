6ffb4117edb4967a1c4af1dd77de355e9cd5909d
Shintaro Sakoda <shintaro.sakoda@tier4.jp>
2024-10-30 16:31:59 +0900

    Fixed dlr
    Signed-off-by: Shintaro Sakoda <shintaro.sakoda@tier4.jp>

--- exec_driving_log_replayer.sh
+++ exec_driving_log_replayer.sh
@@ -1,5 +1,13 @@
 #!/bin/bash
-
 set -eux
 
-dlr simulation run -p localization -l "play_rate:=1.0"
+version=${1:-1}
+
+if [ $version -eq 1 ]; then
+    dlr simulation run -p localization -l "play_rate:=1.0"
+elif [ $version -eq 2 ]; then
+    set +eux
+    source ~/autoware/install/setup.bash
+    set -eux
+    ros2 launch driving_log_replayer_v2 driving_log_replayer_v2.launch.py scenario_path:=$HOME/log_evaluator_data/localization/sample/scenario.yaml
+fi
