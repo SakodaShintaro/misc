7b01e57659fe23ab33759a1e2be9e4126498664d
Shintaro Sakoda <shintaro.sakoda@tier4.jp>
2024-11-15 08:57:44 +0900

    Fixed exec_udp_awsim.sh
    Signed-off-by: Shintaro Sakoda <shintaro.sakoda@tier4.jp>

--- exec_udp_awsim.sh
+++ exec_udp_awsim.sh
@@ -29,4 +29,23 @@ ros2 launch autoware_launch e2e_simulator.launch.xml \
     vehicle_model:=sample_vehicle \
     sensor_model:=awsim_sensor_kit \
     launch_sensing_driver:=true \
-    use_sim_time:=false
+    use_sim_time:=false &
+
+# 立ち上がるまで待つ
+# サービス呼び出しを立ち上がりの確認とする
+ros2 service call /localization/pose_twist_fusion_filter/trigger_node std_srvs/srv/SetBool "{data: false}"
+
+# 初期位置推定のステータスが3になるまで待つ
+while true; do
+    status=$(ros2 topic echo /localization/initialization_state --once | grep -oP 'state: \K\d')
+    if [ $status -eq 3 ]; then
+        break
+    fi
+    sleep 2
+done
+
+# 安定性のため少し待つ
+sleep 5
+
+# ループ開始
+python3 ~/misc/python_lib/goal_manager.py --goals_yaml ~/misc/awsim_goal/goals_long.yaml --loop_num 0
