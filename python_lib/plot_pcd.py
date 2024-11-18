cfa5d7ed335071dd8ee5715aee3c29854d715a86
Shintaro Sakoda <shintaro.sakoda@tier4.jp>
2024-11-18 15:42:17 +0900

    Added pose_tsv arg
    Signed-off-by: Shintaro Sakoda <shintaro.sakoda@tier4.jp>

--- python_lib/plot_pcd.py
+++ python_lib/plot_pcd.py
@@ -6,12 +6,14 @@ import pathlib
 import matplotlib.pyplot as plt
 import numpy as np
 import open3d as o3d
+import pandas as pd
 
 
 def parse_args() -> argparse.Namespace:
     parser = argparse.ArgumentParser()
     parser.add_argument("map_dir", type=pathlib.Path)
     parser.add_argument("--save_path", type=pathlib.Path, default=None)
+    parser.add_argument("--pose_tsv", type=pathlib.Path, default=None)
     parser.add_argument("--by_open3d", action="store_true")
     return parser.parse_args()
 
@@ -20,6 +22,7 @@ if __name__ == "__main__":
     args = parse_args()
     map_dir = args.map_dir
     save_path = args.save_path
+    pose_tsv = args.pose_tsv
     by_open3d = args.by_open3d
 
     pcd_path = args.map_dir / "pointcloud_map.pcd"
@@ -52,6 +55,14 @@ if __name__ == "__main__":
     pcd = pcd.voxel_down_sample(voxel_size=4.0)
     points = np.asarray(pcd.points)
     plt.scatter(points[:, 0], points[:, 1], s=0.5, linewidths=0)
+
+    if pose_tsv is not None:
+        df = pd.read_csv(pose_tsv, delimiter="\t")
+        plt.plot(df["position.x"], df["position.y"], color="orange")
+
     plt.axis("equal")
+    plt.grid()
+    plt.xlabel("x [m]")
+    plt.ylabel("y [m]")
     plt.savefig(save_path, bbox_inches="tight", pad_inches=0.05, dpi=300)
     print(f"Saved to {save_path.resolve()}")
