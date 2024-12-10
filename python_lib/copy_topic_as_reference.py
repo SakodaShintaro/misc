d9f8a3a61d888d6b6bf1e355505802ba42cbf190
Shintaro Sakoda <shintaro.sakoda@tier4.jp>
2024-12-10 14:16:04 +0900

    Fixed copy_topic_as_reference.py
    Signed-off-by: Shintaro Sakoda <shintaro.sakoda@tier4.jp>

--- python_lib/copy_topic_as_reference.py
+++ python_lib/copy_topic_as_reference.py
@@ -1,16 +1,19 @@
+"""Copy a topic from a rosbag to another rosbag as a reference topic."""
+
+import argparse
+import pathlib
+
+import pandas as pd
 import rosbag2_py
 from rclpy.serialization import deserialize_message, serialize_message
 from rosidl_runtime_py.utilities import get_message
-import argparse
-import pathlib
-import pandas as pd
 
 
-def parse_args():
+def parse_args() -> argparse.Namespace:
     parser = argparse.ArgumentParser()
-    parser.add_argument('source1_rosbag_path', type=pathlib.Path)
-    parser.add_argument('source2_rosbag_path', type=pathlib.Path)
-    parser.add_argument('output_rosbag_path', type=pathlib.Path)
+    parser.add_argument("source1_rosbag_path", type=pathlib.Path)
+    parser.add_argument("source2_rosbag_path", type=pathlib.Path)
+    parser.add_argument("output_rosbag_path", type=pathlib.Path)
     parser.add_argument("--target_topic", type=str, default="/localization/kinematic_state")
     parser.add_argument("--output_topic_prefix", type=str, default="reference_")
     return parser.parse_args()
@@ -25,20 +28,18 @@ if __name__ == "__main__":
     output_topic_prefix = args.output_topic_prefix
 
     # read source1 rosbag (get target topic)
-    serialization_format = 'cdr'
-    storage_options = rosbag2_py.StorageOptions(
-        uri=str(source1_rosbag_path), storage_id="sqlite3"
-    )
+    serialization_format = "cdr"
+    storage_options = rosbag2_py.StorageOptions(uri=str(source1_rosbag_path), storage_id="sqlite3")
     converter_options = rosbag2_py.ConverterOptions(
         input_serialization_format=serialization_format,
-        output_serialization_format=serialization_format)
+        output_serialization_format=serialization_format,
+    )
 
     reader = rosbag2_py.SequentialReader()
     reader.open(storage_options, converter_options)
 
     topic_types = reader.get_all_topics_and_types()
-    type_map = {
-        topic_types[i].name: topic_types[i].type for i in range(len(topic_types))}
+    type_map = {topic_types[i].name: topic_types[i].type for i in range(len(topic_types))}
 
     elements = target_topic.split("/")
     output_topic_name = "/".join(elements[:-1] + [output_topic_prefix + elements[-1]])
@@ -46,47 +47,43 @@ if __name__ == "__main__":
     storage_filter = rosbag2_py.StorageFilter(topics=target_topics)
     reader.set_filter(storage_filter)
 
-    data_list = list()
+    data_list = []
     while reader.has_next():
         (topic, data, timestamp_rosbag) = reader.read_next()
         msg_type = get_message(type_map[topic])
         msg = deserialize_message(data, msg_type)
-        timestamp_header = int(msg.header.stamp.sec) + \
-            int(msg.header.stamp.nanosec) * 1e-9
+        timestamp_header = int(msg.header.stamp.sec) + int(msg.header.stamp.nanosec) * 1e-9
         if topic == target_topic:
-            data_list.append({
-                'timestamp_header': timestamp_header,
-                'timestamp_rosbag': timestamp_rosbag,
-                'msg': msg,
-            })
+            data_list.append(
+                {
+                    "timestamp_header": timestamp_header,
+                    "timestamp_rosbag": timestamp_rosbag,
+                    "msg": msg,
+                },
+            )
         else:
-            assert False, f"Unknown topic: {topic}"
+            raise ValueError(f"Unknown topic: {topic}")
     df_data = pd.DataFrame(data_list)
 
     # prepare source2 rosbag (get all topics without target topic)
-    storage_options = rosbag2_py.StorageOptions(
-        uri=str(source2_rosbag_path), storage_id="sqlite3"
-    )
+    storage_options = rosbag2_py.StorageOptions(uri=str(source2_rosbag_path), storage_id="sqlite3")
     reader = rosbag2_py.SequentialReader()
     reader.open(storage_options, converter_options)
     topic_types = reader.get_all_topics_and_types()
-    type_map = {
-        topic_types[i].name: topic_types[i].type for i in range(len(topic_types))
-    }
+    type_map = {topic_types[i].name: topic_types[i].type for i in range(len(topic_types))}
 
     # prepare output rosbag
-    storage_options = rosbag2_py.StorageOptions(
-        uri=str(output_rosbag_path), storage_id="sqlite3"
-    )
+    storage_options = rosbag2_py.StorageOptions(uri=str(output_rosbag_path), storage_id="sqlite3")
     writer = rosbag2_py.SequentialWriter()
     writer.open(storage_options, converter_options)
 
     # create_topic
-    for topic in type_map.keys():
+    for topic in type_map:
         if topic == output_topic_name:
             continue
         topic_info = rosbag2_py.TopicMetadata(
-            name=topic, type=type_map[topic], serialization_format=serialization_format)
+            name=topic, type=type_map[topic], serialization_format=serialization_format
+        )
         writer.create_topic(topic_info)
     topic_info = rosbag2_py.TopicMetadata(
         name=output_topic_name,
@@ -100,7 +97,7 @@ if __name__ == "__main__":
     while reader.has_next():
         (topic, data, timestamp_rosbag) = reader.read_next()
         if topic == output_topic_name:
-            continue    # skip if already exists
+            continue  # skip if already exists
         writer.write(topic, data, timestamp_rosbag)
         if first_timestamp_header is None:
             first_timestamp_header = timestamp_rosbag
@@ -109,9 +106,9 @@ if __name__ == "__main__":
     # write target topic to output rosbag
     add_count = 0
     for _, row in df_data.iterrows():
-        timestamp_header = row['timestamp_header'] * 1e9
-        timestamp_rosbag = row['timestamp_rosbag']
-        msg = row['msg']
+        timestamp_header = row["timestamp_header"] * 1e9
+        timestamp_rosbag = row["timestamp_rosbag"]
+        msg = row["msg"]
         if timestamp_header < first_timestamp_header or last_timestamp_header < timestamp_header:
             continue
 
