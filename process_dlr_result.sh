ddfc35107c09604b4d0d21e66cacb208eb6ffb38
Shintaro Sakoda <shintaro.sakoda@tier4.jp>
2025-02-27 14:53:38 +0900

    Fixed
    Signed-off-by: Shintaro Sakoda <shintaro.sakoda@tier4.jp>

--- process_dlr_result.sh
+++ process_dlr_result.sh
@@ -12,7 +12,8 @@ dir_list=$(find $result_dir -mindepth 1 -maxdepth 1 -type d | sort)
 
 for dir in $dir_list; do
   target_rosbag=$dir/result_bag
-  parent_dir=$(dirname $target_rosbag)
+  compare_result_dir=$(dirname $target_rosbag)/compare_trajectories
+  mkdir -p $compare_result_dir
 
   # plot localization result
   python3 $script_dir/python_lib/plot_localization_result.py $target_rosbag
@@ -24,12 +25,12 @@ for dir in $dir_list; do
   dir_name=$(basename $dir)
   python3 $script_dir/python_lib/extract_pose_from_rosbag.py \
     $target_rosbag \
-    --save_dir $parent_dir/compare_trajectories \
+    --save_dir $compare_result_dir \
     --target_topics "/localization/kinematic_state" \
                     "/localization/reference_kinematic_state"
   python3 $script_dir/python_lib/compare_trajectories.py \
-    $parent_dir/compare_trajectories/localization__kinematic_state.tsv \
-    $parent_dir/compare_trajectories/localization__reference_kinematic_state.tsv
+    $compare_result_dir/localization__kinematic_state.tsv \
+    $compare_result_dir/localization__reference_kinematic_state.tsv
 
   elapsed_time=$(date -ud "@$SECONDS" +"%T")
 done
