ec5c3bd1aac94cb76ca70de498aa6c7bd5e6802f
Shintaro Sakoda <shintaro.sakoda@tier4.jp>
2025-03-10 09:49:56 +0900

    FIxed mount
    Signed-off-by: Shintaro Sakoda <shintaro.sakoda@tier4.jp>

--- docker/run.sh
+++ docker/run.sh
@@ -20,5 +20,5 @@ docker run -it \
     --volume=$HOME/work:$HOME/work \
     --volume=$HOME/data:$HOME/data \
     --volume $HOME/.cache/:$HOME/.cache/ \
-    --volume /media:/media \
+    --volume /media:/media:rwx,shared \
     ${IMAGE_NAME} bash
