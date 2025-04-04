c2a655039e73b3bab3f1a026e0ef4077f8561aec
Shintaro Sakoda <shintaro.sakoda@tier4.jp>
2025-04-04 18:13:00 +0900

    Added rename_topic.py
    Signed-off-by: Shintaro Sakoda <shintaro.sakoda@tier4.jp>

---
+++ python_lib/rename_topic.py
@@ -0,0 +1,54 @@
+"""A script to add bias to imu."""
+
+import argparse
+from pathlib import Path
+
+from rosbags.rosbag2 import Reader, Writer
+
+
+def parse_args() -> argparse.Namespace:
+    parser = argparse.ArgumentParser()
+    parser.add_argument("input_bag", type=Path)
+    parser.add_argument("topic_name_before", type=str)
+    parser.add_argument("topic_name_after", type=str)
+    return parser.parse_args()
+
+
+if __name__ == "__main__":
+    args = parse_args()
+    input_bag = args.input_bag
+    topic_name_before = args.topic_name_before
+    topic_name_after = args.topic_name_after
+
+    output_bag = input_bag.with_name(f"{input_bag.stem}_renamed{input_bag.suffix}")
+
+    reader = Reader(input_bag)
+    writer = Writer(output_bag)
+    reader.open()
+    writer.open()
+
+    topic_name_to_connection = {}
+    for connection in reader.connections:
+        if connection.topic == topic_name_before:
+            topic_name_to_connection[topic_name_after] = writer.add_connection(
+                topic_name_after,
+                connection.msgtype,
+                offered_qos_profiles=connection.ext.offered_qos_profiles,
+            )
+        else:
+            topic_name_to_connection[connection.topic] = writer.add_connection(
+                connection.topic,
+                connection.msgtype,
+                offered_qos_profiles=connection.ext.offered_qos_profiles,
+            )
+
+    for connection, timestamp, rawdata in reader.messages():
+        if connection.topic == topic_name_before:
+            writer.write(topic_name_to_connection[topic_name_after], timestamp, rawdata)
+        else:
+            writer.write(topic_name_to_connection[connection.topic], timestamp, rawdata)
+
+    reader.close()
+    writer.close()
+
+    print(f"Saved to {output_bag}")
