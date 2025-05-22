0d9cef9129e817c5b40ea0e0afbe1673adb7942c
Shintaro Sakoda <shintaro.sakoda@tier4.jp>
2025-05-22 13:01:46 +0900

    Fixed
    Signed-off-by: Shintaro Sakoda <shintaro.sakoda@tier4.jp>

--- python_lib/for_3dgs/prepare_data_from_rosbag.sh
+++ python_lib/for_3dgs/prepare_data_from_rosbag.sh
@@ -5,18 +5,22 @@ rosbag_path=$(readlink -f $1)
 mkdir -p $2
 output_dir=$(readlink -f $2)
 
-pose_topic=/awsim/ground_truth/localization/kinematic_state
+pose_topic=/localization/kinematic_state
 
-# 画像を抽出
-python3 $HOME/misc/python_lib/extract_images_from_rosbag.py \
-  $rosbag_path \
-  $output_dir/images
+set +eux
+source ~/autoware/install/setup.bash
+set -eux
 
 # poseを抽出
 ros2 run autoware_localization_evaluation_scripts extract_values_from_rosbag.py \
   $rosbag_path \
   --target_topics=$pose_topic \
   --save_dir=$output_dir/images
+
+# 画像を抽出
+python3 $HOME/misc/python_lib/extract_images_from_rosbag.py \
+  $rosbag_path \
+  $output_dir/images
 
 # poseをカメラposeに変換
 python3 $HOME/misc/python_lib/calc_camera_pose.py \
