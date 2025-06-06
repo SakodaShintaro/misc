ecc59da9baadafe853f1fb60c13a10f01a435990
Shintaro Sakoda <shintaro.sakoda@tier4.jp>
2025-06-06 09:04:28 +0900

    Added replace_ros_key.sh
    Signed-off-by: Shintaro Sakoda <shintaro.sakoda@tier4.jp>

---
+++ replace_ros_key.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+set -eux
+
+sudo rm /etc/apt/sources.list.d/ros2.list
+sudo rm /usr/share/keyrings/ros-archive-keyring.gpg
+sudo apt update && sudo apt install curl -y
+export ROS_APT_SOURCE_VERSION=$(curl -s https://api.github.com/repos/ros-infrastructure/ros-apt-source/releases/latest | grep -F "tag_name" | awk -F\" '{print $4}')
+curl -L -o /tmp/ros2-apt-source.deb "https://github.com/ros-infrastructure/ros-apt-source/releases/download/${ROS_APT_SOURCE_VERSION}/ros2-apt-source_${ROS_APT_SOURCE_VERSION}.$(. /etc/os-release && echo $VERSION_CODENAME)_all.deb" # If using Ubuntu derivates use $UBUNTU_CODENAME
+sudo apt install /tmp/ros2-apt-source.deb
