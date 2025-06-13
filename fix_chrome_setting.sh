a6df231978ceec830bf6413f31fea466bda38ba1
sakoda <sakoda@keio.jp>
2025-06-13 22:21:22 +0900

    Fixed fix
    Signed-off-by: sakoda <sakoda@keio.jp>

--- fix_chrome_setting.sh
+++ fix_chrome_setting.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
 set -eux
 
+change=${1:-false}
+
 cat /usr/share/applications/google-chrome.desktop | grep Exec
-# sudo sed -i 's|^Exec=/usr/bin/google-chrome-stable|Exec=/usr/bin/google-chrome-stable --gtk-version=3|' /usr/share/applications/google-chrome.desktop
+
+
+if [ "$change" = true ]; then
+    sudo sed -i 's|^Exec=/usr/bin/google-chrome-stable|Exec=/usr/bin/google-chrome-stable --gtk-version=3|' /usr/share/applications/google-chrome.desktop
+fi
