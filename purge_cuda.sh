74017ea9bae6f31749c079a6758a68f5d45d84aa
Shintaro SAKODA <shintaro.sakoda@tier4.jp>
2025-06-17 08:56:16 +0900

    Fixed purge_cuda.sh
    Signed-off-by: Shintaro SAKODA <shintaro.sakoda@tier4.jp>

--- purge_cuda.sh
+++ purge_cuda.sh
@@ -1,12 +1,5 @@
 #!/bin/bash
 set -eux
 
-sudo apt-mark unhold cuda-*
-sudo apt-mark unhold nvidia-*
-sudo apt-mark unhold libcudnn*
-sudo apt-mark unhold libnv*
-
-sudo apt purge -y cuda-*
-sudo apt purge -y nvidia-*
-sudo apt purge -y libcudnn*
-sudo apt purge -y libnv*
+sudo apt-mark unhold cuda-* nvidia-* libcudnn* libnv*
+sudo apt purge -y cuda-* nvidia-* libcudnn* libnv*
