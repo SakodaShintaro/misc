9fc0539569dae70168ebd1aef249aa918430382f
Shintaro SAKODA <shintaro.sakoda@tier4.jp>
2025-06-17 09:34:59 +0900

    確認コマンド追加
    Signed-off-by: Shintaro SAKODA <shintaro.sakoda@tier4.jp>

--- purge_cuda.sh
+++ purge_cuda.sh
@@ -3,3 +3,8 @@ set -eux
 
 sudo apt-mark unhold cuda-* nvidia-* libcudnn* libnv*
 sudo apt purge -y cuda-* nvidia-* libcudnn* libnv*
+
+# 確認
+# ls -la /etc/alternatives/cuda
+# find /usr/lib/x86_64-linux-gnu/ -name "libcudnn.so*"
+# find /usr/lib/x86_64-linux-gnu/ -name "libnvinfer.so*"
