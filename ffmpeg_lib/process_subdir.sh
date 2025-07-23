d4a2da656615a8195ab7495d6b7f6f82d2c0f91e
Shintaro Sakoda <shintaro.sakoda@tier4.jp>
2025-07-23 09:51:52 +0900

    Fixed process_subdir.sh
    Signed-off-by: Shintaro Sakoda <shintaro.sakoda@tier4.jp>

--- ffmpeg_lib/process_subdir.sh
+++ ffmpeg_lib/process_subdir.sh
@@ -5,8 +5,9 @@ set -eux
 target_dir=$(readlink -f $1)
 script_path=$(readlink -f $(dirname $0)/make_mp4_from_unsequential_png.sh)
 
-target_dir_list=$(find $target_dir -mindepth 1 -maxdepth 1 -type d | sort)
+# target_dir以下のすべてのpngファイルの親ディレクトリの集合を作成
+dir_set=$(find "$target_dir" -type f -name '*.png' -exec dirname {} \; | sort | uniq)
 
-for dir in $target_dir_list; do
-    bash $script_path $dir
+for dir in $dir_set; do
+    bash "$script_path" "$dir"
 done
