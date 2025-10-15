f3f9494d5c4c4cf7dbbbdb20410b6689646a9f88
Shintaro SAKODA <shintaro.sakoda@tier4.jp>
2025-10-16 08:44:40 +0900

    Added bkp
    Signed-off-by: Shintaro SAKODA <shintaro.sakoda@tier4.jp>

--- copy_chrome_setting.sh
+++ copy_chrome_setting.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 set -eux
+sudo cp /opt/google/chrome-remote-desktop/chrome-remote-desktop /opt/google/chrome-remote-desktop/chrome-remote-desktop.bak
 sudo cp ./chrome-remote-desktop /opt/google/chrome-remote-desktop/chrome-remote-desktop
